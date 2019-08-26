a498098a4dd1634136e2d9be11007f67f80e872a
sukhcubic <sukhjinderji.singh@gmail.com>
2019-08-26 17:40:03 -0500

    UF Array

---
+++ Java/src/UnionFindArray.java
@@ -0,0 +1,29 @@
+
+public class UnionFindArray {
+
+	public static void main(String args[]) {
+		UnionFindArray uf = new UnionFindArray();
+		// uf.makeSet(1);
+		// uf.makeSet(2);
+		// uf.makeSet(3);
+		// uf.makeSet(4);
+		// uf.makeSet(5);
+		// uf.makeSet(6);
+		// uf.makeSet(7);
+		//
+		// uf.union(1, 2);
+		// uf.union(2, 3);
+		// uf.union(4, 5);
+		// uf.union(6, 7);
+		// uf.union(5, 6);
+		// uf.union(3, 7);
+		//
+		// System.out.println(uf.findRoot(1));
+		// System.out.println(uf.findRoot(2));
+		// System.out.println(uf.findRoot(3));
+		// System.out.println(uf.findRoot(4));
+		// System.out.println(uf.findRoot(5));
+		// System.out.println(uf.findRoot(6));
+		// System.out.println(uf.findRoot(7));
+	}
+}
