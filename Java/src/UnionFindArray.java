3ad21b807ae5e70217be2659633e7b94bfad171a
sukhcubic <sukhjinderji.singh@gmail.com>
2019-08-27 13:17:46 -0500

    UF Array

--- Java/src/UnionFindArray.java
+++ Java/src/UnionFindArray.java
@@ -1,29 +1,65 @@
 
 public class UnionFindArray {
+	int[] uf;
+	int[] sz;
+
+	public void init(int N) {
+		uf = new int[N];
+		sz = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			uf[i] = i;
+		}
+
+	}
+
+	public void union(int a, int b) {
+		int root1 = findRoot(a);
+		int root2 = findRoot(b);
+
+		if (root1 == root2) {
+			return;
+		}
+
+		if (sz[root1] < sz[root2]) {
+			uf[root1] = root2;
+			sz[root2] += sz[root1];
+		} else {
+			uf[root2] = root1;
+			sz[root1] += sz[root2];
+		}
+
+	}
+
+	// Find and compression by pointing to its grandparent. Half the path
+	// length.
+	private int findRoot(int a) {
+
+		while (a != uf[a]) {
+			uf[a] = uf[uf[a]];
+			a = uf[a];
+		}
+		return a;
+
+	}
 
 	public static void main(String args[]) {
 		UnionFindArray uf = new UnionFindArray();
-		// uf.makeSet(1);
-		// uf.makeSet(2);
-		// uf.makeSet(3);
-		// uf.makeSet(4);
-		// uf.makeSet(5);
-		// uf.makeSet(6);
-		// uf.makeSet(7);
+		uf.init(8);
+
+		uf.union(1, 2);
+		uf.union(2, 3);
+		uf.union(4, 5);
+		uf.union(6, 7);
+		uf.union(5, 6);
+		uf.union(3, 7);
 		//
-		// uf.union(1, 2);
-		// uf.union(2, 3);
-		// uf.union(4, 5);
-		// uf.union(6, 7);
-		// uf.union(5, 6);
-		// uf.union(3, 7);
-		//
-		// System.out.println(uf.findRoot(1));
-		// System.out.println(uf.findRoot(2));
-		// System.out.println(uf.findRoot(3));
-		// System.out.println(uf.findRoot(4));
-		// System.out.println(uf.findRoot(5));
-		// System.out.println(uf.findRoot(6));
-		// System.out.println(uf.findRoot(7));
+		System.out.println(uf.findRoot(1));
+		System.out.println(uf.findRoot(2));
+		System.out.println(uf.findRoot(3));
+		System.out.println(uf.findRoot(4));
+		System.out.println(uf.findRoot(5));
+		System.out.println(uf.findRoot(6));
+		System.out.println(uf.findRoot(7));
 	}
 }
