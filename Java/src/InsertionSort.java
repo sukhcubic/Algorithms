04806695d0b9aaf41abdc3e898f4a55b43494046
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-10-12 16:08:10 -0500

    Create InsertionSort.java

---
+++ Java/src/InsertionSort.java
@@ -0,0 +1,27 @@
+public class InsertionSort {
+
+	public static int[] insertionSort(int[] data) {
+
+		for (int i = 1; i < data.length; i++) {
+			int temp = data[i];
+			for (int j = i - 1; j >= 0; j--) {
+				if (temp < data[j]) {
+					int x = data[j];
+					data[j] = temp;
+					data[i] = x;
+				} else {
+					break;
+				}
+			}
+		}
+		return data;
+	}
+
+	public static void main(String[] args) {
+		int[] a = new int[] { 4, 2, 1, 1, 7, 4, 5, 6, 6, 6, 7, 7, 5, 9, 7 };
+		// insertionSort(a);
+		for (int i : insertionSort(a)) {
+			System.out.println(i);
+		}
+	}
+}
