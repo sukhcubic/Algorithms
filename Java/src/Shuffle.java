1df61d38ab166d6151b71b4ed33605f997df12a4
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-10-23 22:07:36 -0500

    Update Shuffle.java

--- Java/src/Shuffle.java
+++ Java/src/Shuffle.java
@@ -1,1 +1,28 @@
 
+import java.util.Random;
+
+public class Shuffle {
+
+	
+	
+	public static int[] shuffle(int [] a) {
+		Random rand = new Random(); 
+
+		
+		for(int i = 1; i < a.length; i++) {
+			int random = rand.nextInt(i);
+			int swap = a[i];
+			a[i] = a[random];
+			a[random] = swap;
+		}
+		
+		return a;
+	}
+	
+	public static void main(String[] args) {
+		int[] a = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		for (int i : shuffle(a)) {
+			System.out.println(i);
+		}
+	}
+}
