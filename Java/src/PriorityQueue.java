87287d534b996e423ef1dbd055406e427ecd052e
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-11-05 23:23:40 -0600

    Update PriorityQueue.java

--- Java/src/PriorityQueue.java
+++ Java/src/PriorityQueue.java
@@ -1,7 +1,10 @@
-
+//Source https://github.com/williamfiset/data-structures/blob/master/com/williamfiset/datastructures/priorityqueue/BinaryHeap.java
+//https://github.com/bephrem1/backtobackswe/blob/master/Sorting%2C%20Searching%2C%20%26%20Heaps/ImplementABinaryHeap/ImplementABinaryHeap.java
+//https://www.youtube.com/watch?v=t0Cq6tVNRBA&list=PLI1t_8YX-Apv-UiRlnZwqqrRT8D1RhriX&index=10
+//https://www.coursera.org/learn/algorithms-part1/lecture/Uzwy6/binary-heaps
 public class PriorityQueue {
 	
-	static int[] a = new int[20];
+	static int[] a = new int[9];
 	static int lastIndex = 0;
 	
 	public static void insert(int element) {
@@ -11,19 +14,32 @@ public class PriorityQueue {
 	}
 	
 	public static void delete() {
-		a[0] =  a[lastIndex--];	
+		a[0] =  a[lastIndex-1];	
 		sink(0);
+		lastIndex--;
 	}
 	
 	private static void swim(int x) {
-		while(x >0 && a[ x] > a[x/2]) {
-			swap(x, x/2);
-			x = x/2;
+		int parent = (x-1)/2;
+		System.out.println("sss");
+
+		while(x >0 && a[x] < a[parent]) {
+
+			swap(parent, x);
+			x = parent;
+			parent = (x-1)/2;
  		}
+		
+		for (int i : a) {
+			System.out.println(i);
+		}
 	}
 	
 	private static void sink(int x) {
+
 		while(x < lastIndex) {
+			System.out.println("dccc"+lastIndex  + "  xxx  "  + x);
+
 			int left = 2*x+1;
 			int right = 2*x+2;
 			int selected = left;
@@ -31,13 +47,17 @@ public class PriorityQueue {
 				selected = right;
 			}
 			
-			if(x > selected) {
+			if(left >= a.length|| left < x) {
 				break;
 			}
-			
 			swap(x, selected);
 			
 			x = selected;
+
+		}
+		System.out.println("dddd");
+		for (int i : a) {
+			System.out.println(i);
 		}
 	}
 	
@@ -54,10 +74,9 @@ public class PriorityQueue {
 		for (int i : a) {
 			insert(i);
 		}
-		
-		for (int i : a) {
-			System.out.println(i);
-		}
+		delete();
 	}
 
 }
+
+
