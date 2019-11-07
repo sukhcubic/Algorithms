c4ee3130d790c4a4ca88771953b2bd726d837138
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-11-06 23:06:35 -0600

    Update FindFixedPoint.java

--- Java/src/FindFixedPoint.java
+++ Java/src/FindFixedPoint.java
@@ -1,3 +1,18 @@
 //A fixed point in a list is where the value is equal to its index. So for example the list [-5, 1, 3, 4], 1 is a fixed point
 //in the list since the index and value is the same. Find a fixed point (there can be many, just return 1) in a sorted list 
 //of distinct elements, or return None if it doesn't exist.
+
+public class FindFixedPoint {
+
+	public static int find(int[] values) {
+		int found = -1;
+		
+		return found;
+	}
+
+	public static void main(String[] args) {
+		int[] a = new int[] { 77, 10, 9, 4, 78, 95, 15, 15,  3};
+
+	}
+	
+}
