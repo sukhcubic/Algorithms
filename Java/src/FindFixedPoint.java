6e70696dd755f6a563e7b7c415df324da045f783
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-11-06 23:37:16 -0600

    Update FindFixedPoint.java

--- Java/src/FindFixedPoint.java
+++ Java/src/FindFixedPoint.java
@@ -3,16 +3,32 @@
 //of distinct elements, or return None if it doesn't exist.
 
 public class FindFixedPoint {
-
+// Fast way is using Binary search which takes log(n) time complexity
 	public static int find(int[] values) {
+		return find(values, 0, values.length-1);
+	}
+	
+	public static int find(int[] values, int start, int end) {
 		int found = -1;
-		
+		if(end>=start) {
+	    int mid = (start+end)/2;
+	    //Check where matching index lies in the sorted array
+	    if(mid == values[mid]) {
+	    	return mid;
+	    }else if(mid > values[mid]){
+	    	return find(values, mid +1, end );
+	    }else {
+	    	return find(values, start,  mid-1);
+	    }
+		}
 		return found;
 	}
+	
 
 	public static void main(String[] args) {
-		int[] a = new int[] { -5, 1, 3, 4};
+		int[] a = new int[] {-10, -1, 0, 3 , 10, 11, 30, 50, 100};
 
+		System.out.print(find(a));
 	}
 	
 }
