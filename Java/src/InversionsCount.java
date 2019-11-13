e59ef2da4e5e370470fc9ae3c4cbd6d0f68f33db
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-11-12 22:46:16 -0600

    Update InversionsCount.java

--- Java/src/InversionsCount.java
+++ Java/src/InversionsCount.java
@@ -1,6 +1,6 @@
 public class InversionsCount{
 	public static void main(String[] args) {
-		int[] a = new int[] {10, 7, 12, 8, 3, 2};
+		int[] a = new int[] {1, 2, 3,0,0};
 		count(a);
 		for (int i : a) {
 			System.out.println(i);
@@ -8,41 +8,30 @@ public class InversionsCount{
 	}
 
 	private static void count(int[] a) {
-		int mid = a.length/2;
-		int secondHalf = a.length - mid;
-		int[] aux1 = new int[mid];
-		int[] aux2 = new int[secondHalf];		
-
-		for (int i= 0; i<a.length; i++ ) {
-			int z = 0;
-			if(i<mid) {
-				aux1[i] = a[i];
-			}else {
-				aux2[z++] = a[i];
-			}
-		}
-		mSort(a, aux1, 0, mid);
-		mSort(a, aux2, mid+1, a.length-1);
+	
+		sort(a);
 		
 	}
-	
+	static int count =0;
 	private static void sort(int[] a) {
 		int[] aux = new int[a.length];
 		mSort(a, aux, 0, a.length-1);
+		System.out.println(count + "  count ");
 	}
-	
-	private static void mSort(int[] a, int[] aux, int lo, int hi) {
-		if(lo>= hi) {
-			return;
+	private static int mSort(int[] a, int[] aux, int lo, int hi) {
+		
+		if(lo>=hi) {
+			return 0;
 		}
 		int m = (lo + hi)/2;
-		mSort(a, aux, lo, m);
-		mSort(a, aux, m+1, hi);
-		merge(a, aux, lo, m+1, hi);
-		
+		count += mSort(a, aux, lo, m);
+		count += mSort(a, aux, m+1, hi);
+	    merge(a, aux, lo, m+1, hi);
+		return mSort(a, aux, lo, m) +  mSort(a, aux, m+1, hi) +   merge(a, aux, lo, m+1, hi);
 	}
 	
-	private static void merge(int[] a, int[] aux, int lo, int mid,  int hi) {
+	private static int merge(int[] a, int[] aux, int lo, int mid,  int hi) {
+		int value = 0;
 		for (int i= 0; i<a.length; i++ ) {
 			aux[i] = a[i];
 		}
@@ -59,7 +48,9 @@ public class InversionsCount{
 				a[i] = aux[l++];
 			}else {
 				a[i] = aux[m++];
+				value++;
 			}
 		}
+		return value;
 	}
 }
