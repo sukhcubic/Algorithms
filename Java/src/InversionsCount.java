695bd67ff7d5ce0acb9f7f3e9f78c8a16997e883
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-11-13 16:40:17 -0600

    Update InversionsCount.java

--- Java/src/InversionsCount.java
+++ Java/src/InversionsCount.java
@@ -2,58 +2,82 @@
 //https://www.geeksforgeeks.org/count-inversions-of-size-k-in-a-given-array/
 //https://www.geeksforgeeks.org/count-inversions-array-set-3-using-bit/
 public class InversionsCount{
-public static void main(String[] args) {
-		int[] a = new int[] {1, 2, 3,0,0};
+
+	public static void main(String[] args) {
+		int[] a = new int[] {1, 20, 6, 4, 5};
 		count(a);
-		for (int i : a) {
-			System.out.println(i);
-		}
+//		for (int i : a) {
+//			System.out.println(i);
+//		}
 	}
 
 	private static void count(int[] a) {
-	
-		sort(a);
-		
+		sort(a);	
 	}
-	static int count =0;
+	
 	private static void sort(int[] a) {
 		int[] aux = new int[a.length];
-		mSort(a, aux, 0, a.length-1);
-		System.out.println(count + "  count ");
+		System.out.println(mSort(a, aux, 0, a.length-1) + "  count ");
 	}
+	
 	private static int mSort(int[] a, int[] aux, int lo, int hi) {
-		
 		if(lo>=hi) {
 			return 0;
 		}
-		int m = (lo + hi)/2;
-		count += mSort(a, aux, lo, m);
-		count += mSort(a, aux, m+1, hi);
-	    merge(a, aux, lo, m+1, hi);
-		return mSort(a, aux, lo, m) +  mSort(a, aux, m+1, hi) +   merge(a, aux, lo, m+1, hi);
+		int m = (lo + hi) / 2;
+		
+		return mSort(a, aux, lo, m) +  mSort(a, aux, m+1, hi) +   merge(a, aux, lo, m, hi);
 	}
 	
 	private static int merge(int[] a, int[] aux, int lo, int mid,  int hi) {
 		int value = 0;
-		for (int i= 0; i<a.length; i++ ) {
+		
+		for (int i= 0; i<=hi; i++ ) {
 			aux[i] = a[i];
 		}
-		int m = mid;
+		
+		int m = mid+1;
 		int l = lo;
-		for(int i = lo; i<= hi; i++) {
-			if(l > mid) {
-				// first half exhausted but we still have some elements left on second half. Copy them over
-				a[i] = aux[m++];
-			}else if(m > hi) {
-				// Second half exhausted but we still have some elements left on first half. Copy them over
-				a[i] = aux[l++];
-			}else if(aux[l] < aux[m] ) {
-				a[i] = aux[l++];
+		int i = lo;
+		
+		while(l <=  mid && m <= hi ) {
+			if(aux[l] > aux[m] ) {
+				a[i++] = aux[m++];
+				value += mid-l; 
 			}else {
-				a[i] = aux[m++];
-				value++;
+				a[i++] = aux[l++];
 			}
 		}
+		
+		while(m <= hi) {
+			a[i++] = aux[m++];
+			value++;
+		}
+		
+		while(l <= mid) {
+			a[i++] = aux[l++];	
+		}
+		
+//		for(int i = lo; i <= hi; i++) {
+//			if(l > mid) {
+//				// first half exhausted but we still have some elements left on second half. Copy them over
+//				a[i] = aux[m++];
+//			}else if(m > hi) {
+//				// Second half exhausted but we still have some elements left on first half. Copy them over
+//				a[i] = aux[l++];
+//			}else if(aux[l] > aux[m] ) {
+//				a[i] = aux[m++];
+//				if(m > l) {
+//				value++;
+//				}
+//			}else {
+//				a[i] = aux[l++];
+//				if(l > m) {
+//				value++;
+//				}
+//			}
+//		}
+		
 		return value;
 	}
 }
