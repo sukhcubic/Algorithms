8fd2d54dce9c6ff1145695e8aee0e9db92e9e6f1
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-11-29 23:04:08 -0600

    Update LinkedListRemoveDuplicates.java

--- Java/src/LinkedListRemoveDuplicates.java
+++ Java/src/LinkedListRemoveDuplicates.java
@@ -26,23 +26,26 @@ public class LinkedListRemoveDuplicates {
   
   // Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
   	private Lnode removeDupii(Lnode head) {
-    	Lnode current = head;
-    	while(current != null && current.next != null) {
-    		//1 2 3 3 4 5
-    		// Checking if current and next will stay same but current.next will point to next.next  and 
-    		// shortened the list but removing current.next reference to current.next.next
-    		if(current.data == current.next.data){
-    			current.next = current.next.next;
-    			//1 2 3 4 5
-    			// we are one updating next reference. current will stay at 3. Once 3 reference is removed then it will
-    			// fall back to else condition which will actually move current pointer.
+    	Lnode ns = new Lnode(0);
+    	ns.next = head;
+    	
+    	Lnode current = ns;
+    	//we are looking ahead two actual node if two next node are same we will delete pointer by pointing next.next
+    	while(current.next != null  && current.next.next  != null ) {
+    		if(current.next.data == current.next.next.data){
+    		 int data = current.next.data ;
+    		 //run until we see different data which is non duplicate
+    		 while(current.next != null && current.next.data == data) {
+    				current.next = current.next.next;
+    			}
     		}
     		else {
+    			//Initially current is 0 since we created new node with placeholder 0 and next with rest of the list. tHen update pointer if there is no duplicates
     			current = current.next;
-    			// updating current so actual pointer can move forward.
     		}
     	}	
-    	return head;
+    	//Discard placeholder node and return original list
+    	return ns.next;
     }
 
 	public void add(int value) {
