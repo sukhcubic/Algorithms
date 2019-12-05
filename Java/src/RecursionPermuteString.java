0218e4a21c452c30a7f6b02e71bb16af222f662d
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-04 22:44:28 -0600

    Update RecursionPermuteString.java

--- Java/src/RecursionPermuteString.java
+++ Java/src/RecursionPermuteString.java
@@ -6,12 +6,8 @@ public class RecursionPermuteString {
 	}
 
 	public static void main(String[] args) {
-		Recursion rec = new Recursion();
-		// rec.printNums(5);
-		// System.out.println(rec.sum(5));
-		// System.out.println(rec.power(3, 3));
-		// System.out.println(rec.fact(3));
-		//System.out.println(rec.add(32134445));
-		System.out.println(rec.countNum(33545445));
+		RecursionPermuteString rec = new RecursionPermuteString();
+	
+		System.out.println(rec.permute(""));
 	}
 }
