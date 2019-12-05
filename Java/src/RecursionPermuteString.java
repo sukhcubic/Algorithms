094e5e9f56f41be6e5ccc6d45c09d7bc87b1a581
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-04 22:43:02 -0600

    Create RecursionPermuteString.java

---
+++ Java/src/RecursionPermuteString.java
@@ -0,0 +1,17 @@
+
+public class RecursionPermuteString {
+	
+	private void permute() {
+		
+	}
+
+	public static void main(String[] args) {
+		Recursion rec = new Recursion();
+		// rec.printNums(5);
+		// System.out.println(rec.sum(5));
+		// System.out.println(rec.power(3, 3));
+		// System.out.println(rec.fact(3));
+		//System.out.println(rec.add(32134445));
+		System.out.println(rec.countNum(33545445));
+	}
+}
