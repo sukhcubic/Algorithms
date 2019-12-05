8986066fcc6871596b172ca9d373bc5521463abb
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-04 22:42:33 -0600

    Create RecursivePalindrome.java

---
+++ Java/src/RecursivePalindrome.java
@@ -0,0 +1,22 @@
+
+public class RecursivePalindrome {
+
+	private boolean isPalindrome(String s) {
+		 if(s.length() <= 1) {
+			 return true;
+		 }else {
+			
+			 if(s.charAt(0) == s.charAt(s.length()-1)) {
+				 return isPalindrome(s.substring(1, s.length()-1));
+			 }else {
+				 return false;
+			 }
+		 }
+	
+	}
+	
+	public static void main(String[] args) {
+		RecursivePalindrome rec = new RecursivePalindrome();
+		System.out.println(rec.isPalindrome("abcba"));
+	}
+}
