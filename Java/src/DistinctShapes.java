05c0efe27c092b5e69ac80b8a5ee35b3044076cc
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-07 20:38:10 -0600

    Update DistinctShapes.java

--- Java/src/DistinctShapes.java
+++ Java/src/DistinctShapes.java
@@ -35,7 +35,6 @@ public class DistinctShapes {
 	}
 
 	public static void main(String args[]) {
-
 		int[][] matrix = { { 0, 1, 0, 0, 0 }, { 0, 1, 1, 1, 0 }, { 1, 1, 0, 0, 1 }, { 0, 1, 1, 1, 1 } };
 		System.out.print(DistinctShapes.numberOfIsland(matrix));
 	}
