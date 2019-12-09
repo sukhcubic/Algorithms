bf9d7818ee5cec18c3f7830708d2079fb68f3ae3
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-09 09:39:49 -0600

    Update Recursion.java

--- Java/src/Recursion.java
+++ Java/src/Recursion.java
@@ -1,6 +1,6 @@
 
 public class Recursion {
-//https://www.youtube.com/watch?v=9f7mjOX4z5A
+	// https://www.youtube.com/watch?v=9f7mjOX4z5A
 	private void printNums(int num) {
 		if (num == 0) {
 			return;
@@ -58,82 +58,75 @@ public class Recursion {
 
 		if (num == count) {
 			return count;
-		}
-		
-		if() {
-		return count;	
-		}
-		else {
+		} else {
 			return x + add(y);
 		}
 	}
 
-		private int countNum(int num) {
-			int x = num % 10;
-			int y = num / 10;
+	private int countNum(int num) {
+		int x = num % 10;
+		int y = num / 10;
 
-			if (num <= 9) {
-				return num == 5 ? 1:0;
-			}else {
-			   return x == 5 ?  1+countNum(y) : countNum(y) ;
-			}	
+		if (num <= 9) {
+			return num == 5 ? 1 : 0;
+		} else {
+			return x == 5 ? 1 + countNum(y) : countNum(y);
 		}
-			
-	
-	         private void printBinary(int num) {
-			  if(num < 0) {
-				  System.out.print("-");
-				  printBinary(-num);
-			  }
-		     else if(num <= 1) {
-				System.out.print(num);
-				return ;
-			}else {
-				int x = num % 2;
-				int y = num / 2;
-			    printBinary(y);
-				//System.out.print(x);
-			   // or
-			    printBinary(x);
+	}
+
+	private void printBinary(int num) {
+		if (num < 0) {
+			System.out.print("-");
+			printBinary(-num);
+		} else if (num <= 1) {
+			System.out.print(num);
+			return;
+		} else {
+			int x = num % 2;
+			int y = num / 2;
+			printBinary(y);
+			// System.out.print(x);
+			// or
+			printBinary(x);
+		}
+	}
+
+	// https://www.bilibili.com/video/av21619854?p=9 starting around 20:00
+	private int evaluateHelper(String expression, int index) {
+		if (Character.isDigit(expression.charAt(index))) {
+			int result = expression.charAt(index++) - '0';
+			return result;
+		} else {
+			index++; // get next value
+
+			int left = evaluateHelper(expression, index);
+
+			char op = expression.charAt(index++);
+
+			int right = evaluateHelper(expression, index);
+			int result;
+			if (op == '*') {
+				result = left * right;
+			} else {
+				result = left + right;
 			}
+			return result;
 		}
-	
-	       		//https://www.bilibili.com/video/av21619854?p=9  starting around 20:00
-		private int evaluateHelper(String expression, int index) {
-			if(Character.isDigit(expression.charAt(index))) {
-				int result = expression.charAt(index++) - '0';
-				return result;
-			}else {	
-				index++; //get next value
-				
-				int left = evaluateHelper(expression, index);
-				
-				char op = expression.charAt(index++);
-				
-				int right = evaluateHelper(expression, index);
-				int result;
-                 if(op == '*') {
-                	 result = left*right;
-                 }else {
-                	 result = left+right;
-                 }
-                  return result;
-		 }			
-		}
-		
-		private int evaluate(String expression) {
-			return evaluateHelper(expression, 0);			
-		}
+	}
 
-		public static void main(String[] args) {
-			Recursion rec = new Recursion();
-			// rec.printNums(5);
-			// System.out.println(rec.sum(5));
-			// System.out.println(rec.power(3, 3));
-			// System.out.println(rec.fact(3));
-			//System.out.println(rec.add(32134445));
-			System.out.println(rec.countNum(33545445));
-			rec.printBinary(-5);
-		}
+	private int evaluate(String expression) {
+		return evaluateHelper(expression, 0);
+	}
+
+	public static void main(String[] args) {
+		Recursion rec = new Recursion();
+		// rec.printNums(5);
+		// System.out.println(rec.sum(5));
+		// System.out.println(rec.power(3, 3));
+		// System.out.println(rec.fact(3));
+		// System.out.println(rec.add(32134445));
+		System.out.println(rec.countNum(33545445));
+		rec.printBinary(-5);
+	}
 }
 
