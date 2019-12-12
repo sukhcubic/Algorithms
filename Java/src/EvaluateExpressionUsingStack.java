6fa3c376e72e36ba520925e7b54105953663e884
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-12 11:34:20 -0600

    Update EvaluateExpressionUsingStack.java

--- Java/src/EvaluateExpressionUsingStack.java
+++ Java/src/EvaluateExpressionUsingStack.java
@@ -1,1 +1,9 @@
+public class EvaluateExpressionUsingStack{
 
+	public static void main(String[] args) {
+		EvaluateExpressionUsingStack rec = new EvaluateExpressionUsingStack();
+		String exp = "(2+(3+4))";
+		String [] val = exp.split("");
+		System.out.println(rec.evaluate(val));
+	}
+}
