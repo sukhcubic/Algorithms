7dffb2ce0de3bd94af7a9c4a5e110be154b07cb7
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-14 12:28:07 -0600

    Update EvaluateExpressionUsingStack.java

--- Java/src/EvaluateExpressionUsingStack.java
+++ Java/src/EvaluateExpressionUsingStack.java
@@ -1,27 +1,30 @@
 public class EvaluateExpressionUsingStack{
 
-	public int evaluate(String s){
-		int result;
+    public int evaluate(String s){
 	        String [] vals = s.split("");
-		Stack<int> ints = new Stack();
-		Stack<String> str = new Stack();
-		for(String val in vals){
-			if(val.eauals"("){
-			   
-			   	
-			}else if(val.equals")"){
-			
-			}else if(val.equasl"+"){
-			
-			}else if(val.equasl"*"){
-			  str.push(val);
+		Stack<Integer> ints = new Stack<Integer>();
+		Stack<String> str = new Stack<String>();
+		for(String val : vals){
+			if(val.equals("(")){  
+				//str.push(val);
+			}else if(val.equals(")")){
+				String op = str.pop();
+				if(op.equals("+")) {
+					ints.push(ints.pop() + ints.pop());
+				}else {
+					ints.push(ints.pop() * ints.pop());	
+					}
+		    }else if(val.equals("+")){
+				 str.push(val);
+			}else if(val.equals("*")){
+			     str.push(val);
 			}else{
-			  ints.push(Integer.parseInt(val););
+			     ints.push(Integer.parseInt(val));
 			}
-		}
-     	
-	     return result;
+		} 
+	     return ints.pop();
 	}
+	
 	public static void main(String[] args) {
 		EvaluateExpressionUsingStack rec = new EvaluateExpressionUsingStack();
 		String exp = "(2+(3+4))";
