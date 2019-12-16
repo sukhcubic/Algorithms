6ed74aad925c1de767e9c64a9b7d59a5850ba53d
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-15 22:26:23 -0600

    Update RecursionPrintBinary.java

--- Java/src/RecursionPrintBinary.java
+++ Java/src/RecursionPrintBinary.java
@@ -1,1 +1,24 @@
+public class RecursionPrintBinary {
 
+	private void printBinary(int digits) {
+		printBinaryHelper(digits, "");			
+	}
+	
+
+	private void printBinaryHelper(int digits, String string) {
+
+		if(digits == 0) {
+			System.out.print(string + "\n");
+		}
+		else {
+			printBinaryHelper(digits -1, string + "0");
+			printBinaryHelper(digits -1, string + "1");
+		}
+	}
+
+
+	public static void main(String[] args) {
+		RecursionPrintBinary rec = new RecursionPrintBinary();
+	    rec.printBinary(10);
+	}
+}
