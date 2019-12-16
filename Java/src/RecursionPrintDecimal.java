7aca7755da7f9f70f40f3a569799d6c43d60a113
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-16 17:02:07 -0600

    Update RecursionPrintDecimal.java

--- Java/src/RecursionPrintDecimal.java
+++ Java/src/RecursionPrintDecimal.java
@@ -5,15 +5,13 @@ public class RecursionPrintDecimal {
 	}
 
 	private void printDeciamlHelper(int digits, String string) {
-		System.out.print(string + "\n");
-
 		if (digits == 0) {
-			// System.out.print(string + "\n");
+			System.out.print(string + "\n");
 		} else {
-    for(int x = 0; x <=9; x++;){
-		  	printDeciamlHelper(digits - 1, string + String.valueOf(x));
-      }
-	}
+			for (int x = 0; x <= 9; x++) {
+				printDeciamlHelper(digits - 1, string + String.valueOf(x));
+			}
+		}
 	}
 
 	public static void main(String[] args) {
