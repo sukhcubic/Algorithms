799664c8426dd84c0cc1f05aa9ec15750126e8c7
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-16 17:00:01 -0600

    Create RecursionPrintDecimal.java

---
+++ Java/src/RecursionPrintDecimal.java
@@ -0,0 +1,23 @@
+public class RecursionPrintDecimal {
+
+	private void printDeciaml(int digits) {
+		printDeciamlHelper(digits, "");
+	}
+
+	private void printDeciamlHelper(int digits, String string) {
+		System.out.print(string + "\n");
+
+		if (digits == 0) {
+			// System.out.print(string + "\n");
+		} else {
+    for(int x = 0; x <=9; x++;){
+		  	printDeciamlHelper(digits - 1, string + String.valueOf(x));
+      }
+	}
+	}
+
+	public static void main(String[] args) {
+		RecursionPrintDecimal rec = new RecursionPrintDecimal();
+		rec.printDeciaml(3);
+	}
+}
