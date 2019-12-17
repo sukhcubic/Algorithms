880a15dd89b0679f0111182840fd5d5644f35af2
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-17 12:57:10 -0600

    Update RecursionDicesRoll.java

--- Java/src/RecursionDicesRoll.java
+++ Java/src/RecursionDicesRoll.java
@@ -19,9 +19,34 @@ public class RecursionDicesRoll {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		rollDicesHelper(numberOfDices, list);
 	}
+	
+	private void sumHelper(int num, int sum, ArrayList<Integer> s) {
+		// System.out.print(" dd d ");
+
+		if (num == 0) {
+			int _sum = 0;
+			for (Integer i : s) {
+				_sum += i;
+			}
+			if (_sum == sum) {
+				System.out.print(s + "\n");
+			}
+		} else {
+			for (int i = 1; i <= 6; i++) {
+				s.add(i);
+				sumHelper(num - 1, sum, s);
+				s.remove(s.size() - 1);
+			}
+		}
+	}
+
+	private void sum(int numberOfDices, int sum) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		sumHelper(numberOfDices, sum, list);
+	}
 
 	public static void main(String[] args) {
 		RecursionDicesRoll rec = new RecursionDicesRoll();
-		rec.rollDices(3);
+		//rec.rollDices(3);
 	}
 }
