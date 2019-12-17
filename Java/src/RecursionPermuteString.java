01545fe4bf6183d7c1193a3e033d0157e2c7f1ce
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-16 21:35:20 -0600

    Update RecursionPermuteString.java

--- Java/src/RecursionPermuteString.java
+++ Java/src/RecursionPermuteString.java
@@ -1,12 +1,27 @@
 
 public class RecursionPermuteString {
-	
-	private void permute(String s) {
+	//https://www.bilibili.com/video/av21619854?p=10  starting at 30:00
+	private void permuteHelper(String s, String pre) {
+		if(s.length() == 0) {
+			System.out.append(pre + "\n");
+		}else {
+			for(int i = 0; i <s.length(); i++) {
+				String sub = s.substring(i, i+1);
+				String ss = s.substring(0, i) + s.substring(i+1);
+				permuteHelper(ss, pre+sub);
+			}
+		}
 		
 	}
+	
+	private void permute(String s) {
+		permuteHelper(s, "");
+	}
+	
+	
 
 	public static void main(String[] args) {
 		RecursionPermuteString rec = new RecursionPermuteString();
-		System.out.println(rec.permute(""));
+		rec.permute("SMARTY");
 	}
 }
