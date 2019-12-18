8b68cf3f44a280d378bd0a94d41a24da9e1b2565
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-18 09:04:48 -0600

    Update RecursionSuperSet.java

--- Java/src/RecursionSuperSet.java
+++ Java/src/RecursionSuperSet.java
@@ -1,47 +1,48 @@
+
 import java.util.ArrayList;
 
 //https://www.bilibili.com/video/av21619854?p=11 at 46:00
 public class RecursionSuperSet {
-	// in this algorith we need to deicede if we need to include in decision treee or not.
+	// in this algorithm we need to decide if we need to include in decision tree
+	// or not.
 	private void subsetHelper(ArrayList<String> s, ArrayList<String> chosen) {
-		if(s.size() == 0) {
-			System.out.print(s);
-		}else {
-		  //select one from list
-		  String nm = s.get(0);
-		
-		  //choose from list
-		  s.remove(0);
-		
-		  //Include in decision tree
-		  chosen.add(nm);
-		  subsetHelper(s, chosen);
-		
-		  //Exclude chosen 
-		
-		  chosen.remove(chosen.size() -1);
-		  subsetHelper(s, chosen);
-		  
-		  //unchoose
-		  s.add(0, nm);
+		// System.out.print(s.size());
+
+		if (s.size() == 0) {
+			System.out.print(chosen + "\n");
+		} else {
+			// select one from list
+			String nm = s.get(0);
+
+			// choose from list
+			s.remove(0);
+
+			// Include in decision tree
+			chosen.add(nm);
+			subsetHelper(s, chosen);
+
+			// Exclude chosen
+
+			chosen.remove(chosen.size() - 1);
+			subsetHelper(s, chosen);
+
+			// unchoose
+			s.add(0, nm);
 		}
-		
+
 	}
-	
+
 	private void subset(ArrayList<String> names) {
 		ArrayList<String> s = new ArrayList<String>();
 		subsetHelper(names, s);
 	}
-	
-	
 
 	public static void main(String[] args) {
 		RecursionSuperSet rec = new RecursionSuperSet();
 		ArrayList<String> names = new ArrayList<String>();
-		names.add("BOB");
-		names.add("DOB");
-		names.add("SAM");
-		names.add("RAM");
+		names.add("B");
+		names.add("D");
+		names.add("S");
 		rec.subset(names);
 	}
 
