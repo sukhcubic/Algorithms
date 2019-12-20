24546d567733d2f53b9a01081fe687e0f8af266b
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-20 17:57:55 -0600

    Update and rename NQueens.java to RecursionNQueens.java

--- Java/src/RecursionNQueens.java
+++ Java/src/RecursionNQueens.java
@@ -1,9 +1,10 @@
-class NQueen{
+class RecursionNQueens{
 // Explanation https://www.youtube.com/watch?v=xFv_Hl4B83A
 
 
   public static void main(String[] args) {
-  
+  RecursionNQueens nq = new RecursionNQueens();
+    nq.find();
   }
 
 }
