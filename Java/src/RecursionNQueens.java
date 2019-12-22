cdc252ef2b836679316f8e163a6f617e2e715317
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-21 22:55:46 -0600

    Update RecursionNQueens.java

--- Java/src/RecursionNQueens.java
+++ Java/src/RecursionNQueens.java
@@ -10,10 +10,29 @@ class RecursionNQueens{
     nq.find();
   }
   
-  private boolean isValid(int row, int col) {
+  private boolean isValid(int board[][], int row, int col) {
 	  
-	  return false;
+	  int i, j; 
+	  
+      /* Check this row on left side */
+      for (i = 0; i < col; i++) 
+          if (board[row][i] == 1) 
+              return false; 
+
+      /* Check upper diagonal on left side */
+      for (i = row, j = col; i >= 0 && j >= 0; i--, j--) 
+          if (board[i][j] == 1) 
+              return false; 
+
+      /* Check lower diagonal on left side */
+      for (i = row, j = col; j >= 0 && i < N; i++, j--) 
+          if (board[i][j] == 1) 
+              return false; 
+
+      return true;
   }
+
+
 
 
   
