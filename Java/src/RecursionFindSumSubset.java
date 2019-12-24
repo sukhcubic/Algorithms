dd92969b2ea560710234b67d5754f312dbd6508c
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-24 15:44:29 -0600

    Update RecursionFindSumSubset.java

--- Java/src/RecursionFindSumSubset.java
+++ Java/src/RecursionFindSumSubset.java
@@ -3,32 +3,35 @@
 //Ref: https://www.tutorialspoint.com/Subset-Sum-Problem
 public class RecursionFindSumSubset {
   //Todo: Fix me
-
-  
-	int[] arr = new int[] {1,2,3,5,7,8,9,10};
-	int N = 5;
-	private void subSet(){
-		subSet(arr.length, 0, "");
-	}
 	
-	private void subSet(int exp, int sum, ) {
-		//System.out.print( " exp " + exp + " val "+ val + "\n");
+int[] arr = new int[] { 1, 2, 3, 5, 7, 8, 9, 10 };
+	int N = 5;
 
-		if(exp == 0 || sum == N) {
-			System.out.print(val + "\n");
-			//return;
-		}else {
-		
-		 for(int i = 0; i < arr.length; i++) {
-			subSet(exp -1, sum + arr[i] );
-		  }
+	private void subSet() {
+		subSet(arr.length, 0);
+	}
+
+	private void subSet(int exp, int sum) {
+		// System.out.print( " exp " + exp + " val "+ val + "\n");
+
+		if (sum == N) {
+			System.out.print(sum + "\n");
+			return;
+		} else {
+
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] < sum || sum < N) {
+					subSet(exp - 1, sum + arr[i]);
+				}
+			}
 		}
 	}
-	
+
 	public static void main(String arg[]) {
 		RecursionFindSumSubset rsum = new RecursionFindSumSubset();
-		
+
 		rsum.subSet();
-	
+
 	}
+
 }
