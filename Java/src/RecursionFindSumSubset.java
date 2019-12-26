285594e725a659e232cae519d7be1e134b54ef12
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-25 21:58:29 -0600

    Update RecursionFindSumSubset.java

--- Java/src/RecursionFindSumSubset.java
+++ Java/src/RecursionFindSumSubset.java
@@ -6,6 +6,7 @@
 //https://iq.opengenus.org/subset-sum-problem-backtracking/
 
 public class RecursionFindSumSubset {
+	//This problem is extenion of superset problem. we are only choosing desired results in this case.
   //Todo: Fix me
 	int[] arr = new int[] { 1, 2, 3, 5, 7, 8, 9, 10 };
 	int N = 5;
@@ -23,6 +24,20 @@ public class RecursionFindSumSubset {
 			subSet(i +1, sum + arr[i], s + String.valueOf(arr[i]) );
 		  }
 	       }
+	}
+	//Another option
+	private void subSet(int start, int sum, String s) {
+		System.out.print(sum + "  sum  and s= "+ s + "\n");
+
+		if(start == arr.length) {
+			return;
+		}
+		if (sum == N) {
+			System.out.print(s + "\n");
+		} else {
+			subSet(start+1, sum + arr[start], s + String.valueOf(arr[start]) );
+			subSet(start+1, sum , s  );			
+		}
 	}
 
 
