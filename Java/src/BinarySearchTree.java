046009529c2b34f7af9107b6e482c82985a9c0ef
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2019-12-30 14:48:35 -0600

    Update BinarySearchTree.java

--- Java/src/BinarySearchTree.java
+++ Java/src/BinarySearchTree.java
@@ -1,1 +1,35 @@
 
+
+public class BinarySearchTree {
+
+	class Node {
+		int key;
+		Node left, right;
+
+		public Node(int item) {
+			key = item;
+			left = right = null;
+		}
+	}
+
+	Node root;
+
+	void insert(int val) {
+		insertNode(root, val);
+	};
+
+	Node insertNode(Node root, int val) {
+		if (root == null) {
+			root = new Node(val);
+			return root;
+		}
+
+		if (val > root.key) {
+			root.right = insertNode(root.right, val);
+		} else {
+			root.left = insertNode(root.left, val);
+		}
+		return root;
+
+	}
+}
