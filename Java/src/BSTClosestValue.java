d2dedbc5e143b87433e1592bb5c2d78454e471bb
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-01-23 10:26:33 -0600

    Update BSTClosestValue.java

--- Java/src/BSTClosestValue.java
+++ Java/src/BSTClosestValue.java
@@ -1,19 +1,70 @@
 // https://www.algoexpert.io/questions/Find%20Closest%20Value%20In%20BST
 //Find closest value assum ehere will be only one closest value.
 class BSTClosestValue{
-  public static int findClosestValueInBst(BST tree, int target) {
-    // Write your code here.
-    return -1;
-  }
+    class Node {
+        int key;
+        Node left, right;
 
-  static class BST {
-    public int value;
-    public BST left;
-    public BST right;
-
-    public BST(int value) {
-      this.value = value;
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
     }
-  }
+    Node root;
 
+    void insert(int val) {
+        root = insertNode(root, val);
+    };
+
+    Node insertNode(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (val > root.key) {
+            root.right = insertNode(root.right, val);
+        } else {
+            root.left = insertNode(root.left, val);
+        }
+        return root;
+    }
+
+    private int lastCloset;
+
+    public int findClosest( Node tree, int target) {
+
+       // lastCloset = tree.key;
+
+        if(tree == null){
+            return -1;
+        }
+        
+        if(tree.key == target){
+            return tree.key;
+        }else if(tree.key > target){
+            return findClosest(tree.left, target);
+        }else if(tree.key < target){
+            return findClosest(tree.right, target);
+        }else{
+
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        BSTClosestValue bst = new BSTClosestValue();
+        bst.insert(10);
+        bst.insert(8);
+        bst.insert(7);
+        bst.insert(9);
+        bst.insert(5);
+        bst.insert(15);
+        bst.insert(11);
+        bst.insert(13);
+        bst.insert(14);
+        bst.insert(16);
+        bst.insert(17);
+        System.out.print(bst.findClosest(bst.root, 5));
+    }
 }
