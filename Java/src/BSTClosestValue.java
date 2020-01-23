8ab7579995b7ff3234c766a97040f88ffcf50851
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-01-22 22:40:10 -0600

    Update BSTClosestValue.java

--- Java/src/BSTClosestValue.java
+++ Java/src/BSTClosestValue.java
@@ -1,4 +1,5 @@
-
+// https://www.algoexpert.io/questions/Find%20Closest%20Value%20In%20BST
+//Find closest value assum ehere will be only one closest value.
 class BSTClosestValue{
   public static int findClosestValueInBst(BST tree, int target) {
     // Write your code here.
