eeb8646401d71f7053a2a393438da361ba3554e2
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-01-22 22:38:54 -0600

    Update BSTClosestValue.java

--- Java/src/BSTClosestValue.java
+++ Java/src/BSTClosestValue.java
@@ -1,6 +1,18 @@
 
 class BSTClosestValue{
+  public static int findClosestValueInBst(BST tree, int target) {
+    // Write your code here.
+    return -1;
+  }
 
+  static class BST {
+    public int value;
+    public BST left;
+    public BST right;
 
+    public BST(int value) {
+      this.value = value;
+    }
+  }
 
 }
