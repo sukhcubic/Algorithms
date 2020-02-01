76578d4b79474ff40b9f893998b14cfa4488c32e
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-01-31 22:19:51 -0600

    Update BSTIsValid.java

--- Java/src/BSTIsValid.java
+++ Java/src/BSTIsValid.java
@@ -1,1 +1,31 @@
+class BSTIsValid {
+  public static boolean validateBst(BST tree) {
 
+		if(tree == null){
+			return true;
+		}
+    return isValid(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+	
+	public static boolean isValid(BST tree, int max, int min){
+		
+		if(tree == null){
+			return true;
+		}
+		
+		if(tree.value < min || tree.value >= max){
+			return false;
+		}
+		return isValid(tree.left, min, tree.value ) && isValid(tree.right, tree.value, max);
+	}
+
+  static class BST {
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
+}
