a5e8081d45479ab9871994e80648dfb8e67f9444
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-01 18:22:06 -0600

    Update BstTraversal.java

--- Java/src/BstTraversal.java
+++ Java/src/BstTraversal.java
@@ -1,10 +1,23 @@
 
 class BstTraveral{
+   class BST {
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
   
+  public List<BST> inOrder(BST tree, List<BST> list){
   
+    return null;
+  }
   
   public static void main(String arg[]){
-  
+    BstTraversal bst = new BstTraversal();
+    
   
   }
   
