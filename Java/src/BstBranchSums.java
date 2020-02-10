f9edd414c6f639dfb426585a1ee6a900908d6a1e
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-09 22:19:59 -0600

    Update BstBranchSums.java

--- Java/src/BstBranchSums.java
+++ Java/src/BstBranchSums.java
@@ -2,14 +2,25 @@ class BstBranchSums{
  
   public static List<Integers> findSum(BST node){
   
-     return helper(node, new List<Integers>);
-  
+   List<Integer>  sum = new ArrayList<Integer>();
+   helper(node, sum, 0);
+   return sum;
+   
   }
  
- List<Integers> helper(BST node, List<Integers>  vals ){
- 
- 
-   return vals;
+  public static void helper(BST node, List<Integers>  vals , int sum){
+   
+   if(node == null){
+    return;
+   }
+   int newSum = sum + node.value;
+   if(node.left == null || node.right == null){
+     vals.add(sum);
+     return;
+   }
+
+   helper(node.left, vals, newSum);
+   helper(node.right, vals, newSum);
  }
 
 }
