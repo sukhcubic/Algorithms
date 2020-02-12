e1130ae26e23a938dde42d840f2efe890e5e6468
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-12 10:26:23 -0600

    Update BstMaxPathSum.java

--- Java/src/BstMaxPathSum.java
+++ Java/src/BstMaxPathSum.java
@@ -1,22 +1,52 @@
 class BstMaxPathSum{
   //https://medium.com/algorithm-and-datastructure/binary-tree-maximum-path-sum-3e5a0353a55a
   //https://www.youtube.com/watch?v=p8P4Iv1rrtg
-   public int sum(BstNode root){
-     
-     if(root == null){
-        return 0;
-     }
-   
-     int leftSum = sum(root.left);
-     int rightSum = sum(root.right);
-     
-     int branchSum = Math.max(Math.max(leftSum, rightSum) + root.value, root.value);
-     
-     int branchTop = Math.max(branchSum, leftSum+rightSum+branchSum );
-     
-    
-   }
-
   
+//    int sum = Integer.MIN_VALUE;
+//    public int sum(BstNode root){
+     
+//      if(root == null){
+//         return 0;
+//      }
+   
+//      int leftSum = sum(root.left);
+//      int rightSum = sum(root.right);
+     
+//      int currentSum = Math.max(Math.max(leftSum, rightSum) + root.value, root.value);
+     
+//      sum = Math.max(sum, Math.max(currentSum, leftSum+rightSum+root.value );
+     
+//      return currentSum;
+   
+//    }
+  
+  
+  // Leet code submitted
+  
+   int sum = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        
 
+        helper(root);
+        
+        
+        return sum;
+    }
+    
+    public int helper(TreeNode root){
+        
+        if(root ==null){
+            return 0;
+        }
+        
+        int left = helper(root.left);
+        int right = helper(root.right);
+        
+        int current =  Math.max(Math.max(left, right) + root.val, root.val);
+        
+        sum = Math.max(sum, Math.max(current,  left+right+root.val));
+          
+        return current;
+    }
+                    
 }
