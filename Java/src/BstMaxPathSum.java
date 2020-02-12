f4fe2147050dae608bdbf3b1af1c743345a5a85b
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-11 21:58:11 -0600

    Update BstMaxPathSum.java

--- Java/src/BstMaxPathSum.java
+++ Java/src/BstMaxPathSum.java
@@ -1,5 +1,6 @@
 class BstMaxPathSum{
   //https://medium.com/algorithm-and-datastructure/binary-tree-maximum-path-sum-3e5a0353a55a
+  //https://www.youtube.com/watch?v=p8P4Iv1rrtg
    public void sum(BstNode root){
    
    
