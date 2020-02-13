4309b7bb6a67542a73da6a42a0415450332608a0
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-12 21:58:54 -0600

    Update BstKSumPath.java

--- Java/src/BstKSumPath.java
+++ Java/src/BstKSumPath.java
@@ -1,8 +1,25 @@
 class BstKSumPath{
 
-   void kSumPath(){
+   void kSumPath(Node root, int k){
    
-   
+        if(root == null){
+            return;
+        }
+
+        list.add(root.key);
+        kSumPath(root.left, k);
+        kSumPath(root.right, k);
+        int target =0;
+        String wrongWay = "";
+      //or use stack
+        for (int j = list.size() - 1; j >= 0; j--) {
+            target+= list.get(j);
+            wrongWay += Integer.toString(list.get(j)) + " " ;
+            if(target == k) {
+            	System.out.print(" result  " + wrongWay + "\n");
+            }
+        }
+        list.remove(list.size()-1);   
    }
 
 }
