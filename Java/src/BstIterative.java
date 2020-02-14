e91174f09d39477a4dadf7f53afe4157328bdcea
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-14 10:51:34 -0600

    Update BstIterative.java

--- Java/src/BstIterative.java
+++ Java/src/BstIterative.java
@@ -17,7 +17,9 @@ class BstIterative{
            //3. curr = 3 and curr.left  = null now it will go to else and pop 4. curr = 4 now and will print 4 
            //4  curr = curr.right will make curr 5 and will go to if close since curr != null and push 5 on stack and curr.let is null
            //5  will go to else pop 5 and print value 
-           // stack goes like this 6>4>3  6>4  6  6>5  
+           // stack goes like this 6>4>3  6>4  6  6>5 6 8>7 8 9 
+           // stack all left because the are in decreasing order from root.Stack will add then in incrermental order while right
+           // elements should pop right away because they are al;ready in increasing order
             if(curr != null){
                stack.push(root);
                curr = curr.left;
@@ -28,5 +30,29 @@ class BstIterative{
                curr = curr.right; 
             }
         }
+    }
+    
+    
+    public void iterateV2(Node root){
+        
+    	Node current = root;
+    	Stack<Node> stack = new Stack<Node>();
+
+    	while(true) {
+    		// Stack all left elemnts and the pop in order to set them in ascending order.
+    		while(current != null) {
+      		  stack.push(current);
+      		  current = current.left;
+    		}
+    		if(stack.isEmpty()) {
+    			return;
+    		}
+
+    		current = stack.pop();
+    		System.out.print(current.key + "\n");
+    		current = current.right;	
+    		
+    	}
+        
     }
 }
