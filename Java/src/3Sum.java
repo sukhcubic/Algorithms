5f46880f5c5663e0971ba3764e11d696861f2949
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-14 19:47:14 -0600

    Update 3Sum.java

--- Java/src/3Sum.java
+++ Java/src/3Sum.java
@@ -24,40 +24,6 @@ public class ThreeSum {
 
 		return "Not Found";
 	}
-
-		private static void sumOfThreeNumbers2(int[] list, int desired) {
-		// There are no duplicates
-	        // one way of doing
-
-		Map<Integer, String> map = new HashMap<Integer, String>();
-		for(Integer ints : list ) {
-			map.put(ints, "");
-		}
-                 int last = list.length-1;
-		for(int a= 0; a<list.length; a++) {
-			if(a<last) {
-				int x = list[a];
-				int y = list[last--];
-				if(x+y > desired) {
-					continue;
-				}else {
-					int z =desired - (x+y);
-					map.remove(x);
-					map.remove(y);
-
-					if(map.containsKey(z)) {
-						System.out.print( "found " + x+ " " + y +" "+ z+ "\n ");
-					}
-					map.put(x, "");
-					map.put(y, "");
-				}
-			}else {
-				break;
-			}
-			
-		}
-
-	}
 	
 	public static void main(String[] args) {
 		int[] a = new int[] { 2, 4, 5, 6, 7, 8, 9 };
