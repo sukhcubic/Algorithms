48638fb66d49ce94706a97709d53509b29df88e3
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-14 22:35:21 -0600

    Update 3Sum.java

--- Java/src/3Sum.java
+++ Java/src/3Sum.java
@@ -25,6 +25,33 @@ public class ThreeSum {
 		return "Not Found";
 	}
 	
+	private static void  sumOfThreeNumbers1(int[] list, int desired) {
+
+		//Sort if not already sorted. Sorted array decrease overall runtime o(n)2 
+		Arrays.sort(list);
+		int x = 0;
+		int y = 0;
+		for(int a= 0; a<list.length-2; a++) {
+			x = a+1;
+			y= list.length-1;
+			while(x < y) {
+				
+				if(list[a]+list[x]+list[y] == desired) {
+					System.out.print("   match "+  list[a] + " "+ list[x] + " "+ list[y] +" "+ "\n");
+					x++;
+					y--;
+					// if sum is less than desired then increase left by one because array is sorted and left>right is ascending order and will pick small element.
+					//otherwise decrease right which is higher number.
+				} else if (list[a] + list[x] + list[y] < desired) {
+					x++;
+				} else {
+					y--;
+				}
+			}
+		}
+
+	}
+	
 	public static void main(String[] args) {
 		int[] a = new int[] { 2, 4, 5, 6, 7, 8, 9 };
 		int desiredSum = 20;
