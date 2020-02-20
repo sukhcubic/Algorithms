5f8179207e96c030bc0ef63c563e94701d0bf103
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-20 09:30:04 -0600

    Update LinkedListClone.java

--- Java/src/LinkedListClone.java
+++ Java/src/LinkedListClone.java
@@ -3,17 +3,38 @@ class LinkedListClone{
  //https://www.youtube.com/watch?v=EHpS2TBfWQg
  //https://www.geeksforgeeks.org/clone-linked-list-next-random-pointer-o1-space/
   //Todo
-  public Node clone(Node root){
-    Node parent = root;
-    Node next = null
-    while(current != null){
-      next = current.next;
-      Node clone = new Node(current.data);
-      clone.next = current.next;
-      current = next;
+  class Node {
+        int key;
+        Node random, next;
+
+        public Node(int item) {
+            key = item;
+            random = next = null;
+        }
     }
-    
-  }
-  
+
+    public Node clone(Node head){
+        Node root = head;
+        Node  next = null;
+
+        // Inserting new node in middle of existing list
+        while(head != null){
+            next = head.next;
+            Node inserted = new Node(head.key);
+            head.next = inserted;
+            inserted.next = next;
+            head = next;
+        }
+
+        //reset pointer back to head
+        head =  root;
+
+        //point all random to inserted.next
+        while(head != null){
+            
+        }
+
+        return  root;
+    }
   
 }
