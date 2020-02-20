94b27fb05e0f3ffe1792fa7454d1a7fc30a07cd3
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-19 22:25:10 -0600

    Update LinkedListClone.java

--- Java/src/LinkedListClone.java
+++ Java/src/LinkedListClone.java
@@ -1,6 +1,7 @@
 class LinkedListClone{
  //https://www.youtube.com/watch?v=tCROpuE3EpQ
-//https://www.youtube.com/watch?v=EHpS2TBfWQg
+ //https://www.youtube.com/watch?v=EHpS2TBfWQg
+ //https://www.geeksforgeeks.org/clone-linked-list-next-random-pointer-o1-space/
   //Todo
   public Node clone(Node root){
     Node parent = root;
