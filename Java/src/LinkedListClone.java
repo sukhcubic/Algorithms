e20fbeed1e1278b6bc1fa091b529991c2ccad0ed
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-19 22:23:38 -0600

    Update LinkedListClone.java

--- Java/src/LinkedListClone.java
+++ Java/src/LinkedListClone.java
@@ -1,6 +1,18 @@
 class LinkedListClone{
-//Good Explanation: https://www.youtube.com/watch?v=EHpS2TBfWQg
-  
+ //https://www.youtube.com/watch?v=tCROpuE3EpQ
+//https://www.youtube.com/watch?v=EHpS2TBfWQg
+  //Todo
+  public Node clone(Node root){
+    Node parent = root;
+    Node next = null
+    while(current != null){
+      next = current.next;
+      Node clone = new Node(current.data);
+      clone.next = current.next;
+      current = next;
+    }
+    
+  }
   
   
 }
