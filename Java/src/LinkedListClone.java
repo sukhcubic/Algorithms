f3e2b2c1baed21ffaade72cfe1f5e8069ec6a7d0
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-20 12:24:52 -0600

    Update LinkedListClone.java

--- Java/src/LinkedListClone.java
+++ Java/src/LinkedListClone.java
@@ -2,8 +2,8 @@ class LinkedListClone{
  //https://www.youtube.com/watch?v=tCROpuE3EpQ
  //https://www.youtube.com/watch?v=EHpS2TBfWQg
  //https://www.geeksforgeeks.org/clone-linked-list-next-random-pointer-o1-space/
-  //Todo
-  class Node {
+  
+    static class Node {
         int key;
         Node random, next;
 
@@ -31,10 +31,67 @@ class LinkedListClone{
 
         //point all random to inserted.next
         while(head != null){
-            
+            head.next.random =  head.random.next;
+//            if(head.next != null){
+//                head = head.next.next;
+//            }else{
+//                head = head.next;
+//            }
+            head = (head.next != null) ? head.next.next: head.next;
         }
 
-        return  root;
+        head = root;
+        // reset all the pointers so we have two separate lists
+        // one way of doing
+//        Node temp = new Node(0);
+//        Node cloned = temp;
+//        while(head != null){
+//            temp.next = head.next;
+//            temp = temp.next;
+//            head = (head.next != null) ? head.next.next: head.next;
+//        }
+//        return  cloned.next;
+
+        
+        //.Other way of doing
+        Node temp = head.next;
+        Node clone = temp;
+        while(head != null &&  temp != null ){
+              // if temp.next is not null we are pointing temp node if null we are pointing null
+              head.next = (head.next != null)? head.next.next:head.next;
+              temp.next = (temp.next != null)? temp.next.next:temp.next;
+//              if(temp.next != null){
+//                  temp.next = temp.next.next;
+//              }else{
+//                  temp.next = temp.next; // which is null
+//              }
+              head = head.next;
+              temp = temp.next;
+        }
+        return clone;
+    }
+
+    public static void main(String[] args) {
+
+        CloneLinkedList a = new CloneLinkedList();
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        head.random = head.next.next;
+        head.next.random = head;
+        head.next.next.random = head.next.next.next.next;
+        head.next.next.next.random = head.next.next.next.next;
+        head.next.next.next.next.random = head.next;
+        Node clone = a.clone(head);
+
+        while(clone != null){
+            System.out.println( "  clone data "+ clone.key);
+            clone = clone.next;
+        }
     }
   
 }
