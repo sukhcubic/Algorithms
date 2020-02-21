64aa7a31f6d9e94c0e490de953bb42dfdd96b08c
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-21 16:06:50 -0600

    Update BinaryTreeSerializeDeserialize.java

--- Java/src/BinaryTreeSerializeDeserialize.java
+++ Java/src/BinaryTreeSerializeDeserialize.java
@@ -1,15 +1,21 @@
 class BinaryTreeSerializeDeserialize{
+    //https://www.youtube.com/watch?v=suj1ro8TIVY
+    //https://www.youtube.com/watch?v=lUnNK9jPg2Y
+    
+//     StringBuilder sb = new StringBuilder();
+//     public String serialize(Node root){
+//          if(root == null){
+//              return null;
+//          }
+//          serialize(root.left);
+//          serialize(root.right);
+//          sb.append(root.key + ",");
+//          return sb.toString();
+//     }
 
-    StringBuilder sb = new StringBuilder();
-    public String serialize(Node root){
-         if(root == null){
-             return null;
-         }
-         serialize(root.left);
-         serialize(root.right);
-         sb.append(root.key + ",");
-         return sb.toString();
-    }
+  public String serialize(Node root){
+  
+  }
 
   public Node Deserialize(String tree){
   
