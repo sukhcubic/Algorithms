a48dae3809e71b15f0393d755a1e08efefaae35e
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-20 22:27:26 -0600

    Update BinarySearch.java

--- Java/src/BinarySearch.java
+++ Java/src/BinarySearch.java
@@ -1,7 +1,7 @@
 class BinarySearch{
   
   public boolean search(int num, int[] nums){
-     return 
+     return helper(nums, 0, nums.length(), num);
    }
 
   private boolean helper(int[] nums, int begin, int end, int num){
@@ -16,4 +16,24 @@ class BinarySearch{
      return false;
    }
   }
+  
+   public boolean search1(int num, int[] nums){
+      
+     int low = 0;
+     int high = nums.length()-1;
+     
+     while(low <= high){
+       int mid = (low+high)/2;
+       
+         if(nums[mid]== num){
+              return true;
+          }else if(nums[mid]> num){
+              low = mid+1;
+          }else{
+              high = mid-1;
+          }
+      }
+      return false;
+   }
+  
 }
