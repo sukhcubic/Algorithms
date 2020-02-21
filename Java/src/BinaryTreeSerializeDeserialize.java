eebd548a201144bd15eba7f786b9fc90302a4359
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-21 14:22:09 -0600

    Update BinaryTreeSerializeDeserialize.java

--- Java/src/BinaryTreeSerializeDeserialize.java
+++ Java/src/BinaryTreeSerializeDeserialize.java
@@ -1,5 +1,19 @@
 class BinaryTreeSerializeDeserialize{
 
+  public String Serialize(Node root){
+  
+    if(root == null){
+     return null;
+    }
+    
+    Serialize(root.left);
+    Serialize(root.right);
+    
+  }
 
+  public Node Deserialize(String tree){
+  
+  
+  }
 
 }
