50c61a2aae5d50af96e521031710f536602d9eed
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-21 23:14:46 -0600

    Update BinaryTreeSerializeDeserialize.java

--- Java/src/BinaryTreeSerializeDeserialize.java
+++ Java/src/BinaryTreeSerializeDeserialize.java
@@ -12,14 +12,83 @@ class BinaryTreeSerializeDeserialize{
 //          sb.append(root.key + ",");
 //          return sb.toString();
 //     }
+	
+	  class Node {
+	        int key;
+	        Node left, right;
 
-  public String serialize(Node root){
-  
-  }
+	        public Node(int item) {
+	            key = item;
+	            left = right = null;
+	        }
+	    }
+	    Node root;
 
-  public Node Deserialize(String tree){
-  
-  
-  }
+	    void insert(int val) {
+	        root = insertNode(root, val);
+	    };
 
+	    Node insertNode(Node root, int val) {
+	        if (root == null) {
+	            root = new Node(val);
+	            return root;
+	        }
+	        if (val > root.key) {
+	            root.right = insertNode(root.right, val);
+	        } else {
+	            root.left = insertNode(root.left, val);
+	        }
+	        return root;
+	    }
+
+	  public String serialize(Node root){
+		  if(root == null) {
+			  return "x";
+		  }
+		  
+		  String a = serialize(root.left);
+		  String b = serialize(root.right);
+		  
+		  return String.valueOf(root.key) + "," + a+ ","+ b;
+	  }
+
+	  public Node Deserialize(String tree){
+		  Queue<String> queue = new LinkedList<>();
+		  queue.addAll(Arrays.asList(tree.split(",")));
+	   return null;
+	  }
+	  
+	  private Node helper(Queue que) {
+		  String val = que.poll();
+		  
+		  if(val.equals("x")) {
+			  return null;
+		  }
+		  Node node = new Node(Integer.parseInt(val));
+		  
+		  node.left = helper(que);
+		  node.right = helper(que);
+		  
+		  return node;
+	  }
+	  
+      public static void main(String args[]) {
+    	  BinaryTreeSerializeDeserialize bst = new BinaryTreeSerializeDeserialize();
+          bst.insert(10);
+          bst.insert(8);
+          bst.insert(7);
+          bst.insert(9);
+          bst.insert(5);
+          bst.insert(15);
+          bst.insert(11);
+          bst.insert(13);
+          bst.insert(14);
+          bst.insert(16);
+          bst.insert(17);
+//          for ( int val: bst.rightSideView(bst.root)) {
+//              System.out.println("  rightside view " + val);
+//          }
+          
+          System.out.print(bst.serialize(bst.root));
+      }
 }
