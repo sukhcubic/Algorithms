5e97d534606aa6f5b38fe8cd319e6c941fc7ffda
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-22 22:37:29 -0600

    Update FinadAllDuplicatesLeet442.java

--- Java/src/FinadAllDuplicatesLeet442.java
+++ Java/src/FinadAllDuplicatesLeet442.java
@@ -1,7 +1,6 @@
 class FinadAllDuplicatesLeet442{
 //https://leetcode.com/problems/find-all-duplicates-in-an-array/
       public List<Integer> findDuplicates(int[] nums) {
-        
         List<Integer> dups = new ArrayList<>();
         //Elements are 1 ≤ a[i] ≤ n (n = size of array) there is no zero and max element is <= array.length;
         // Approach is to visit first element and get its value and subtract -1 becuase array is zero based but 
@@ -9,7 +8,6 @@ class FinadAllDuplicatesLeet442{
         // visited. Next time if it sees if value is less than zero (-ve) that mease we have duplicate because we already 
         // visited there.
         for(int i = 0; i < nums.length; i++){
-            
             int x = Math.abs(nums[i])-1;
             if(nums[x] <0){
                 dups.add(x+1);
@@ -18,8 +16,5 @@ class FinadAllDuplicatesLeet442{
             }
         }
       return dups;
-
     }
-
-
 }
