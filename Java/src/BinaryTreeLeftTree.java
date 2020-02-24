8a1be15bc82d1199351bdeb35f6f5a10cc7887c8
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-24 08:46:02 -0600

    Update BinaryTreeLeftTree.java

--- Java/src/BinaryTreeLeftTree.java
+++ Java/src/BinaryTreeLeftTree.java
@@ -1,4 +1,28 @@
 class BinaryTreeLeftTree{
 
+      public List<BstNode> leftSubtree(BstNode root){
+        List<BstNode> list  = new ArrayList<>();
+        if(root == null){
+            return null;
+        }
+        Queue<BstNode> q = new LinkedList<>();
+        if(root.left != null){
+         q.add(root.left);
+        }
 
+        while(!q.isEmpty()){
+            int sz = q.size();
+            for (int i = 0; i<sz; i++ ) {
+                BstNode node = q.remove();
+                list.add(node);
+                if(node.left != null){
+                    q.add(node.left);
+                }
+                if(node.right != null){
+                    q.add(node.right);
+                }
+            }
+        }
+        return list;
+    }
 }
