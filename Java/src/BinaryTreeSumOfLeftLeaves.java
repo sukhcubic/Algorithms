ecbaa6b940852a8ceb698f3e2ff825448798bf11
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-23 21:29:47 -0600

    Update BinaryTreeSumOfLeftLeaves.java

--- Java/src/BinaryTreeSumOfLeftLeaves.java
+++ Java/src/BinaryTreeSumOfLeftLeaves.java
@@ -9,13 +9,37 @@ class BinaryTreeSumOfLeftLeaves{
 		}
         
         if(node.left != null && node.left.left == null && node.left.right == null) {
-		// explore right brancg as well and add value from right tree left leaf
+		// explore right branch as well and add value from right tree left leaf
 			return node.left.val + sumOfLeftLeaves(node.right);
 		}
 		return sumOfLeftLeaves(node.left)+sumOfLeftLeaves(node.right);
     }
 
-
+// Using stack
+	
+	public int sumOfLeftLeaves(Node node) {
+	  if(node == null) return 0;
+			
+	  Stack<Node> stack = new Stack<Node>();
+	  stack.add(node);
+	  
+	  int sum = 0;
+	  while(!stack.isEmpty()) {
+		  Node nd = stack.pop();
+		  
+		  if(nd.left != null && nd.left.left == null && nd.left.right == null) {
+			  sum += nd.left.key;
+		  }
+		  if(nd.left != null) {
+			  stack.add(nd.left);
+		  }
+		  
+		  if(nd.right != null) {
+			  stack.add(nd.right);
+		  }
+	  }
+	  return sum;
+    }
 
 // 	public int sumOfLeaves(Node root) {
 		
