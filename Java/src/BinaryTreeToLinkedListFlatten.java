e57a0fa0d65c8f1cca10efa5cd5b433c71b53bfd
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-26 13:29:31 -0600

    Update BinaryTreeToLinkedListFlatten.java

--- Java/src/BinaryTreeToLinkedListFlatten.java
+++ Java/src/BinaryTreeToLinkedListFlatten.java
@@ -23,6 +23,37 @@ class BinaryTreeToLinkedListFlatten{
         return list;
     }
 
+  
+  //Another approach from LeetCode
+  //https://leetcode.com/problems/flatten-binary-tree-to-linked-list/solution/
+  //recursive approach
+   public void flatten(TreeNode root) {
+        flattenTree(root);
+    }
+    
+       private TreeNode flattenTree(TreeNode node) {
+        
+        if (node == null) {
+            return null;
+        }
+ 
+        if (node.left == null && node.right == null) {
+            return node;
+        }
+        
+        TreeNode leftTail = this.flattenTree(node.left);
+        
+        TreeNode rightTail = this.flattenTree(node.right);
+        
+        if(node.left != null){
+           leftTail.right = node.right;
+            node.right = node.left;
+            node.left = null;
+            
+        }
+         //Return the rightmost we are adjusting right.
+        return   rightTail == null ? leftTail:rightTail;
+    }
 
 
 }
