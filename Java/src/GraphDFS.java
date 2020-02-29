38cee89c24ca672b9a4c02de2dff730a19b51628
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-29 16:25:24 -0600

    Update GraphDFS.java

--- Java/src/GraphDFS.java
+++ Java/src/GraphDFS.java
@@ -1,4 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 class GraphDFS{
+//https://www.youtube.com/watch?v=9RHO6jU--GU
+
+  static class Node {
+        String name;
+        List<Node> children = new ArrayList<Node>();
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public List<String> depthFirstSearch(List<String> array) {
+            Stack<Node> stack = new Stack<>();
+            stack.push(this);
+
+            while(!stack.isEmpty()){
+                Node node = stack.pop();
+                array.add(node.name);
+                while(node.children.size()>0){
+                    Node child = node.children.get(0);
 
 
+                }
+            }
+
+            return array;
+        }
+
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
+    }
 }
