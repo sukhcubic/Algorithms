193606e92fad8fc685f6ab6bc7e05095543046dc
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-02-29 22:47:38 -0600

    Update GraphDFS.java

--- Java/src/GraphDFS.java
+++ Java/src/GraphDFS.java
@@ -5,6 +5,7 @@ class GraphDFS{
 //https://www.youtube.com/watch?v=TIbUeeksXcI DFS BFS
 //https://www.youtube.com/watch?v=zaBhtODEL0w DFS BFS
 //https://www.udemy.com/course/datastructurescncpp/learn/lecture/13193676#overview
+ //https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/tutorial/
   static class Node {
         String name;
         List<Node> children = new ArrayList<Node>();
@@ -13,24 +14,30 @@ class GraphDFS{
             this.name = name;
         }
 
-        public List<String> depthFirstSearch(List<String> array) {
+     public List<String> depthFirstSearch(List<String> array) {
             Stack<Node> stack = new Stack<>();
             stack.push(this);
 
             while(!stack.isEmpty()){
                 Node node = stack.pop();
                 array.add(node.name);
-                while(node.children.size()>0){
-                    Node child = node.children.get(0);
-
-
+                for(Node child:node.children) {
+//                 	array.add(child.name);
+                	stack.add(child);
                 }
             }
-
             return array;
         }
 
-        public Node addChild(String name) {
+//https://www.algoexpert.io/questions/Depth-first%20Search    
+     public List<String> depthFirstSearch(List<String> array) {
+            array.add(node.name);
+                for(int i =0; i<children.length; i++) {
+                  children.get(i).depthFirstSearch(array);
+                 }
+            return array;
+        }
+    public Node addChild(String name) {
             Node child = new Node(name);
             children.add(child);
             return this;
