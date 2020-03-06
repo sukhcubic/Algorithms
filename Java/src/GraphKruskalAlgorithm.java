b3d3caa704204ede44a4497097dfcc500f06bccf
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-03-06 15:53:25 -0600

    Update GraphKruskalAlgorithm.java

--- Java/src/GraphKruskalAlgorithm.java
+++ Java/src/GraphKruskalAlgorithm.java
@@ -1,4 +1,5 @@
 //https://www.hackerearth.com/practice/algorithms/graphs/minimum-spanning-tree/tutorial/
+//https://www.hackerearth.com/blog/developers/kruskals-minimum-spanning-tree-algorithm-example
  class GraphKruskalAlgorithm{
  
  
