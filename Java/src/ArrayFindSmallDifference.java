696d34c87ca3bf252bbac7acfcf38ead38603e2c
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-03-12 22:56:22 -0500

    Update ArrayFindSmallDifference.java

--- Java/src/ArrayFindSmallDifference.java
+++ Java/src/ArrayFindSmallDifference.java
@@ -4,6 +4,28 @@ class ArrayFindSmallDifference{
         int[] result = new int[2];
         Arrays.sort(a);
         Arrays.sort(b);
+        int curr = Integer.MAX_VALUE;
+        int saved = Integer.MAX_VALUE;
+
+        int pointA = 0;
+        int pointB = 0;
+        while(pointA<a.length && pointB<b.length){
+            int x = a[pointA];
+            int y = b[pointB];
+            if(x < y){
+                curr = x-y;
+                pointA++;
+            }else if(x> y){
+                curr = x-y;
+                pointB++;
+            }else{
+                return
+            }
+
+            if(saved < curr){
+                saved
+            }
+        }
      return result;
     }
 
@@ -12,6 +34,8 @@ class ArrayFindSmallDifference{
         int[] a = {-1,5,10,20,28,3};
         int[] b = {26,134,135,15,17};
 
+        //-1 3 5 10 20 28
+        //15 17 26 134 135
         for (int val: find(a, b)){
             System.out.println("  result = "+val);
         }
