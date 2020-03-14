6e1a8de8970388078b2b89756627bade85170ecf
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-03-13 19:16:25 -0500

    Update ArrayFindSmallDifference.java

--- Java/src/ArrayFindSmallDifference.java
+++ Java/src/ArrayFindSmallDifference.java
@@ -1,7 +1,6 @@
 class ArrayFindSmallDifference{
 
-    public static int[] find(int[] a, int[] b){
-        int[] result = new int[2];
+  int[] result = new int[2];
         Arrays.sort(a);
         Arrays.sort(b);
         int curr = Integer.MAX_VALUE;
@@ -9,25 +8,31 @@ class ArrayFindSmallDifference{
 
         int pointA = 0;
         int pointB = 0;
+
+        int[] results = new int[2];
         while(pointA<a.length && pointB<b.length){
             int x = a[pointA];
             int y = b[pointB];
+            //Move pointer until y is bigger than x
             if(x < y){
-                curr = x-y;
+                curr = Math.abs(x-y);
                 pointA++;
-            }else if(x> y){
-                curr = x-y;
+            }else if(x > y){
+                curr = Math.abs(x-y);
                 pointB++;
             }else{
-                return
+                // this means  both values are same diff is zero
+                return new int[]{x, y};
             }
 
-            if(saved < curr){
-                saved
+            //save the previous result
+            if(curr < saved){
+                saved = curr;
+                results = new int[]{x, y};
             }
         }
-     return result;
-    }
+     return results;
+  }
 
     public static void main(String arg []){
 
