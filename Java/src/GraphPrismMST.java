e5d8d9aae67d314eb6edade4c129089dd928822f
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-03-29 23:12:00 -0500

    Update GraphPrismMST.java

--- Java/src/GraphPrismMST.java
+++ Java/src/GraphPrismMST.java
@@ -4,14 +4,53 @@
 //https://algorithms.tutorialhorizon.com/prims-minimum-spanning-tree-mst-using-adjacency-list-and-priority-queue-without-decrease-key-in-oelogv/
 //https://www.coursera.org/learn/algorithms-part2/lecture/HoHKu/prims-algorithm
 class GraphPrismMST{
-	List<Edge<Integer>> resultSet = new ArrayList<>();
-	PriorityQueue<Vertex<Integer>> queue = new PriorityQueue<Vertex<Integer>>();
-	public List<Edge<Integer>>findMST(Graph graph){
 	
-		return resultSet;
-	}
-	
-	
+    public List<Edge<Integer>>findMST(Graph graph){
+        List<Edge<Integer>> result = new ArrayList<>();
+        BinaryMinHeap<Vertex<Integer>> minHeap = new BinaryMinHeap<Vertex<Integer>>();
+
+        Map<Vertex<Integer>,Edge<Integer>> vertexToEdge = new HashMap<>();
+
+        for(Object v : graph.getAllVertex()){
+            minHeap.add(Integer.MAX_VALUE, (Vertex) v);
+        }
+
+        Vertex<Integer> startVertex = (Vertex<Integer>) graph.getAllVertex().iterator().next();
+
+        //for the start vertex decrease the value in heap + map to 0
+        minHeap.decrease(startVertex, 0);
+
+        //iterate till heap + map has elements in it
+        while(!minHeap.empty()){
+            //extract min value vertex from heap + map
+            Vertex<Integer> current = minHeap.extractMin();
+
+            //get the corresponding edge for this vertex if present and add it to final result.
+            //This edge wont be present for first vertex.
+            Edge<Integer> spanningTreeEdge = vertexToEdge.get(current);
+            if(spanningTreeEdge != null) {
+                result.add(spanningTreeEdge);
+            }
+
+            //iterate through all the adjacent vertices
+            for(Edge<Integer> edge : current.getEdges()){
+                Vertex<Integer> adjacent = getVertexForEdge(current, edge);
+                //check if adjacent vertex exist in heap + map and weight attached with this vertex is greater than this edge weight
+                if(minHeap.containsData(adjacent) && minHeap.getWeight(adjacent) > edge.getWeight()){
+                    //decrease the value of adjacent vertex to this edge weight.
+                    minHeap.decrease(adjacent, edge.getWeight());
+                    //add vertex->edge mapping in the graph.
+                    vertexToEdge.put(adjacent, edge);
+                }
+            }
+        }
+        return result;
+    }
+
+    private Vertex<Integer> getVertexForEdge(Vertex<Integer> v, Edge<Integer> e){
+        return e.getVertex1().equals(v) ? e.getVertex2() : e.getVertex1();
+    }
+
     public static void main(String args[]){
         Graph<Integer> graph = new Graph<>(false);
 
@@ -25,12 +64,12 @@ class GraphPrismMST{
         graph.addEdge(3, 5, 5);
         graph.addEdge(3, 6, 4);
 
-        GraphPrismMST prims = new GraphPrismMST();
+        GraphPrimsMST prims = new GraphPrimsMST();
         Collection<Edge<Integer>> edges = prims.findMST(graph);
         for(Edge<Integer> edge : edges){
             System.out.println(edge);
         }
-	    
+
     }
 
 }
