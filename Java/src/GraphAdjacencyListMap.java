82b7d74cdbe82fbf5e7bab9e23db1654279ffbb9
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-06-25 23:14:29 -0500

    Update GraphAdjacencyListMap.java

--- Java/src/GraphAdjacencyListMap.java
+++ Java/src/GraphAdjacencyListMap.java
@@ -1,38 +1,36 @@
-
 import java.util.*;
 
-//Credit Tushar Roy. Code taken for Education purpose only. Ref: https://github.com/mission-peace/interview/blob/master/src/com/interview/graph/Graph.java#L132
-
+//Credit to Tushar Roy. Code taken for Education purpose only. Ref: https://github.com/mission-peace/interview/blob/master/src/com/interview/graph/Graph.java#L132
 public class GraphAdjacencyListMap<T> {
 
     private List<Edge<T>> allEdges;
-    private Map<Long,Vertex<T>> allVertex;
+    private Map<Long, Vertex<T>> allVertex;
     boolean isDirected = false;
 
-    public GraphAdjacencyListMap(boolean isDirected){
+    public GraphAdjacencyListMap(boolean isDirected) {
         allEdges = new ArrayList<Edge<T>>();
-        allVertex = new HashMap<Long,Vertex<T>>();
+        allVertex = new HashMap<Long, Vertex<T>>();
         this.isDirected = isDirected;
     }
-    
-      public void addEdge(long id1, long id2){
-        addEdge(id1,id2,0);
+
+    public void addEdge(long id1, long id2) {
+        addEdge(id1, id2, 0);
     }
 
     //This works only for directed graph because for undirected graph we can end up
     //adding edges two times to allEdges
-    public void addVertex(Vertex<T> vertex){
-        if(allVertex.containsKey(vertex.getId())){
+    public void addVertex(Vertex<T> vertex) {
+        if (allVertex.containsKey(vertex.getId())) {
             return;
         }
         allVertex.put(vertex.getId(), vertex);
-        for(Edge<T> edge : vertex.getEdges()){
+        for (Edge<T> edge : vertex.getEdges()) {
             allEdges.add(edge);
         }
     }
 
-    public Vertex<T> addSingleVertex(long id){
-        if(allVertex.containsKey(id)){
+    public Vertex<T> addSingleVertex(long id) {
+        if (allVertex.containsKey(id)) {
             return allVertex.get(id);
         }
         Vertex<T> v = new Vertex<T>(id);
@@ -40,9 +38,60 @@ public class GraphAdjacencyListMap<T> {
         return v;
     }
 
-    public Vertex<T> getVertex(long id){
+    public Vertex<T> getVertex(long id) {
         return allVertex.get(id);
     }
+
+    public void addEdge(long id1, long id2, int weight) {
+        Vertex<T> vertex1 = null;
+        if (allVertex.containsKey(id1)) {
+            vertex1 = allVertex.get(id1);
+        } else {
+            vertex1 = new Vertex<T>(id1);
+            allVertex.put(id1, vertex1);
+        }
+        Vertex<T> vertex2 = null;
+        if (allVertex.containsKey(id2)) {
+            vertex2 = allVertex.get(id2);
+        } else {
+            vertex2 = new Vertex<T>(id2);
+            allVertex.put(id2, vertex2);
+        }
+
+        Edge<T> edge = new Edge<T>(vertex1, vertex2, isDirected, weight);
+        allEdges.add(edge);
+        vertex1.addAdjacentVertex(edge, vertex2);
+        if (!isDirected) {
+            vertex2.addAdjacentVertex(edge, vertex1);
+        }
+
+    }
+
+    public List<Edge<T>> getAllEdges() {
+        return allEdges;
+    }
+
+    public Collection<Vertex<T>> getAllVertex() {
+        return allVertex.values();
+    }
+
+    public void setDataForVertex(long id, T data) {
+        if (allVertex.containsKey(id)) {
+            Vertex<T> vertex = allVertex.get(id);
+            vertex.setData(data);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        for (Edge<T> edge : getAllEdges()) {
+            buffer.append(edge.getVertex1() + " " + edge.getVertex2() + " " + edge.getWeight());
+            buffer.append("\n");
+        }
+        return buffer.toString();
+    }
+}
 
     class Vertex<T> {
         long id;
@@ -187,7 +236,6 @@ public class GraphAdjacencyListMap<T> {
             return "Edge [isDirected=" + isDirected + ", vertex1=" + vertex1
                     + ", vertex2=" + vertex2 + ", weight=" + weight + "]";
         }
-
+   
     }
 
-}
