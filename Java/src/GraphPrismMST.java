fb6e9d09e627374ea05e258e2a5aec3dab8e6579
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-07-03 16:06:25 -0500

    Update GraphPrismMST.java

--- Java/src/GraphPrismMST.java
+++ Java/src/GraphPrismMST.java
@@ -74,5 +74,92 @@ class GraphPrismMST{
         }
 
     }
+	
+	
+	    static class ResultSet {
+        int parent;
+        int weight;
+    }
+
+    public ResultSet[] primMST(GraphWeighted graph){
+
+        int vertices = graph.vertices;
+        boolean[] mst = new boolean[vertices];
+        ResultSet[] results = new ResultSet[vertices];
+        int [] key = new int[vertices];  //keys used to store the key to know whether priority queue update is required
+
+        //Initialize all the keys to infinity and
+        //initialize resultSet for all the vertices
+        for (int i = 0; i <vertices ; i++) {
+            key[i] = Integer.MAX_VALUE;
+            results[i] = new ResultSet();
+        }
+
+        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>(vertices, new Comparator<Pair<Integer, Integer>>() {
+            @Override
+            public int compare(Pair<Integer, Integer> o1, Pair<Integer, Integer> o2) {
+                return o1.getKey() - o2.getKey();
+            }
+        });
+
+        key[0] = 0;
+        Pair<Integer, Integer> p0 = new Pair<>(key[0], 0);
+        pq.offer(p0);
+
+        results[0]  = new ResultSet();
+        results[0].parent = -1;
+
+        while(!pq.isEmpty()){
+            //extract the min
+            Pair<Integer, Integer> extractedPair = pq.poll();
+
+            //extracted vertex
+            int extractedVertex = extractedPair.getValue();
+            mst[extractedVertex] = true;
+
+            LinkedList<GraphWeighted.Edge> list =  graph.adjacencylist[extractedVertex];
+
+            for (int i = 0; i <list.size() ; i++) {
+                GraphWeighted.Edge edge = list.get(i);
+
+                if(mst[edge.destination]== false){
+                    int destination = edge.destination;
+                    int newKey = edge.weight;
+
+                    if(key[destination]>newKey) {
+                        //add it to the priority queue
+                        Pair<Integer, Integer> p = new Pair<>(newKey, destination);
+                        pq.offer(p);
+                        //update the resultSet for destination vertex
+                        results[destination].parent = extractedVertex;
+                        results[destination].weight = newKey;
+                        //update the key[]
+                        key[destination] = newKey;
+                    }
+                }
+            }
+
+        }
+
+     return results;
+    }
+    
+    public static void main(String args[]){
+        int vertex = 6;
+        GraphWeighted graph = new GraphWeighted(vertex);
+        graph.addEdge(0, 1, 4);
+        graph.addEdge(0, 2, 3);
+        graph.addEdge(1, 2, 1);
+        graph.addEdge(1, 3, 2);
+        graph.addEdge(2, 3, 4);
+        graph.addEdge(3, 4, 2);
+        graph.addEdge(4, 5, 6);
+
+        GraphPrimsMST prims = new GraphPrimsMST();
+        ResultSet[] edges = prims.primMST(graph);
+        for(ResultSet edge : edges){
+            System.out.println(edge.parent);
+        }
+   }
 
 }
