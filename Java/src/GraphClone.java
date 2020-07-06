80ef5b8889bd0554b1addb34b005f5d23996a3f6
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-07-05 23:41:53 -0500

    Update GraphClone.java

--- Java/src/GraphClone.java
+++ Java/src/GraphClone.java
@@ -1,5 +1,46 @@
-class GraphClone{
+import java.util.*;
 
+public class GraphClone {
 
+    //BFS
+    class Node {
+        public int val;
+        public List<Node> neighbors;
 
+        public Node() {}
+
+        public Node(int _val, List<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    public Node cloneBFS(Node node){
+        HashMap<Node, Node> visited = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+
+        q.add(node);
+
+        while(!q.isEmpty()){
+            Node node1 = q.remove();
+
+            for (Node node2:node1.neighbors){
+                if(!visited.containsKey(node2)){
+                    visited.put(node2, new Node(node2.val, new ArrayList()));
+                    q.add(node2);
+                }
+                visited.get(node2).neighbors.add(node2);
+            }
+        }
+        return visited.get(node);
+    }
+
+    
+    //DFS
+    public Node cloneDFS(Node node){
+        return null;
+    
+    }
+
+    
 }
