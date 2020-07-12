af0b0ab08fe2303a6cb4abcc318acdf5d1e92315
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-07-12 17:06:00 -0500

    Create MazeProblem.java

---
+++ Java/src/MazeProblem.java
@@ -0,0 +1,78 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//https://medium.com/@codingfreak/maze-problems-in-data-structures-8530af4d6c8
+
+public class MazeProblem {
+
+
+    //Assume start is 0,0 otherswise has to pass start index
+    private void findPath(int[][] maze) {
+        int row = 0, col = 0;
+        Map<String, Boolean> path = new HashMap();
+        boolean haspath = helper(maze, row, col, path);
+        System.out.println( "Has path = " + haspath);
+        //return path;
+    }
+
+    private boolean helper(int[][] maze, int row, int column, Map<String, Boolean> path){
+        System.out.println(" row = "+ row + " col "+ column);
+
+        if(row == 3 && column == 3 ){
+            System.out.println( "Has path = " );
+
+            return true;
+        }
+
+        if(hasPath(maze,row, column)) {
+            System.out.println(" row = result "+ row + " col "+ column);
+            //visited
+            maze[row][column] = 0;
+
+            //Down
+            if(helper(maze, row + 1, column, path)){
+                return true;
+            };
+
+            //Right
+            if(helper(maze, row, column + 1, path)){
+                return true;
+            };
+
+            //Up
+            if(helper(maze, row - 1, column, path)){
+                return true;
+            };
+
+            //Left
+            if(helper(maze, row, column - 1, path)){
+                return true;
+            };
+
+        }
+
+        return false;
+    }
+
+    private boolean hasPath(int[][] maze, int row, int column){
+        if (row < 0 || column < 0 || row >= maze.length || column >= maze[row].length || maze[row][column] == 0) return false;
+        else return true;
+    }
+
+    public static void main(String args[]){
+        MazeProblem maze = new MazeProblem();
+        int [][] mz = {{1,0,1,0},
+                       {1,1,1,0},
+                       {0,0,1,0},
+                       {0,0,1,1}};
+        maze.findPath(mz);
+//        Iterable<String> path = maze.findPath(mz);
+//        for (String p: path) {
+//            System.out.println( p + " ");
+//        }
+    }
+}
