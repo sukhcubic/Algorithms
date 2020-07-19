4dc26c1a1f26452a0c8bbc0e156363f37b17cff7
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-07-19 18:42:39 -0500

    Update GraphFloodFill-BooleanPaintMatrix.java

--- Java/src/GraphFloodFill-BooleanPaintMatrix.java
+++ Java/src/GraphFloodFill-BooleanPaintMatrix.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class GraphFloodFill-BooleanPaintMatrix{
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         Queue<int[]> queue = new LinkedList<>();
