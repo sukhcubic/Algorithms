9d0eb026ee809469d456e51cbc1d7cb6c410ad7c
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-07-26 00:16:50 -0500

    Update GraphWordSearch.java

--- Java/src/GraphWordSearch.java
+++ Java/src/GraphWordSearch.java
@@ -1,5 +1,19 @@
 //https://leetcode.com/problems/word-search-ii/
 //https://leetcode.com/problems/word-search/
 class GraphWordSearch{
+     public boolean exist(char[][] board, String word) {
+
+        return false;
+    }
+
+    public static void main(String args[]){
+        char board[][] = {{'a', 's', 'c'},
+                          {'a', 'o', 'c' },
+                          {'a', 'n', 'u'}};
+        
+        GraphWordSearch graphWordSearch = new GraphWordSearch();
+        graphWordSearch.exist(board, "sonu");
+    }
+  
 
 }
