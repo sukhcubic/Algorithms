32f8a420d8ab8ee5b0e4e35993e71861f0684d86
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-08-17 23:04:50 -0500

    Update DynamicRodCutting.java

--- Java/src/DynamicRodCutting.java
+++ Java/src/DynamicRodCutting.java
@@ -1,6 +1,16 @@
 public class DynamicRodCutting {
 
+    static int cutRod(int[] price, int len){
+        if (len <= 0)
+            return 0;
+        int maxVal = Integer.MIN_VALUE;
+        return maxVal;
+    }
+    
     public static void main(String args[]){
 
+        int arr[] = new int[] {1, 5, 8, 9, 10, 17, 17, 20};
+        int size = arr.length;
+        System.out.println(cutRod(arr, size));
     }
 }
