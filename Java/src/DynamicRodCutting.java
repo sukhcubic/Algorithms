d91137877c4df51cd9ca4b6abbe1dd44c22d7245
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-08-18 22:30:23 -0500

    Update DynamicRodCutting.java

--- Java/src/DynamicRodCutting.java
+++ Java/src/DynamicRodCutting.java
@@ -1,5 +1,9 @@
 public class DynamicRodCutting {
 //Ref:https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
+//Exp: https://www.youtube.com/watch?v=RYPsOJmhwgE
+//https://web.stanford.edu/class/archive/cs/cs161/cs161.1168/lecture12.pdf
+//https://www.codesdope.com/course/algorithms-rod-cutting/
+//http://www.cs.ust.hk/mjg_lib/Classes/COMP3711H_Fall16/lectures/IntroDP_Handout.pdf
     static int cutRod(int[] price, int len){
         if (len <= 0)
             return 0;
