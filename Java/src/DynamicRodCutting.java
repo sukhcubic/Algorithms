0fed62095d97e24ce2278eb887b79bb3f516bc61
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-08-20 22:36:22 -0500

    Update DynamicRodCutting.java

--- Java/src/DynamicRodCutting.java
+++ Java/src/DynamicRodCutting.java
@@ -34,9 +34,48 @@ public class DynamicRodCutting {
    
    
    //Optimal solution bottom up
-   //TODO: need more work
-     static int cutRod1(int price[],int n)
-    {
+//      static int cutRod1(int price[],int n)
+//     {
+//         int val[] = {1,1,1,1,1,1,1,1,1};
+//         int len[] = new int[n+1];
+//         val[0] = 0;
+
+//         // Build the table val[] in bottom up manner and return
+//         // the last entry from the table
+//         for (int i = 1; i<=n; i++)
+//         {
+//             int max_val = Integer.MIN_VALUE;
+
+//             for (int j = 1; j < i; j++) {
+//                 int temp  = price[j] + val[i - j - 1];
+//                 if(temp>max_val){
+//                     max_val = temp;
+//                     len[i] = j;
+//                 }
+
+//             }
+//             val[i] = max_val;
+//         }
+
+//         for (int a:len
+//              ) {
+//             System.out.println(" a="+ a);
+//         }
+//         Set<Integer> set = new HashSet<>();
+
+//          while (n > 1){
+//              set.add(len[n]);
+//              n = n - len[n];
+//          }
+//         System.out.println(" assww=");
+//         for (int a:set
+//         ) {
+//             System.out.println(" aww="+ a);
+//         }
+//         return val[n];
+//     }
+
+   static  Set<Integer> cutRod1(int price[],int n){
         int val[] = {1,1,1,1,1,1,1,1,1};
         int len[] = new int[n+1];
         val[0] = 0;
@@ -57,29 +96,22 @@ public class DynamicRodCutting {
             }
             val[i] = max_val;
         }
-
-        for (int a:len
-             ) {
-            System.out.println(" a="+ a);
-        }
         Set<Integer> set = new HashSet<>();
 
          while (n > 1){
              set.add(len[n]);
              n = n - len[n];
          }
-        System.out.println(" assww=");
-        for (int a:set
-        ) {
-            System.out.println(" aww="+ a);
-        }
-        return val[n];
+        return set;
     }
-
+   
     public static void main(String args[]){
 
         int arr[] = new int[] {0, 5, 8, 9, 10, 17, 17, 20};
         int size = arr.length;
-        System.out.println(cutRod(arr, size));
+        //System.out.println(cutRod(arr, size));
+        for (int in:cutRod1(arr, size)) {
+            System.out.println(in);
+        }
     }
 }
