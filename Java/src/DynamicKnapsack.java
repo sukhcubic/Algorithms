98ff0c2b1c9342d5277d77ffe7bc0b4f8ab01c53
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-08-22 18:03:59 -0500

    Update DynamicKnapsack.java

--- Java/src/DynamicKnapsack.java
+++ Java/src/DynamicKnapsack.java
@@ -1,3 +1,7 @@
 class DynamicKnapsack{
+  //Types of Knapsack: https://www.youtube.com/watch?v=l02UxPYRmCQ
+  //1. 0/1 full item
+  //2. Fraction
+  //3. Unbounded  same item can be added multiple times
 
 }
