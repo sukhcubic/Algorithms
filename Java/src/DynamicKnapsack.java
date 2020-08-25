bcceea4ca475f9340281fb272b9158c604922e53
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-08-24 22:39:54 -0500

    Update DynamicKnapsack.java

--- Java/src/DynamicKnapsack.java
+++ Java/src/DynamicKnapsack.java
@@ -19,20 +19,77 @@ class DynamicKnapsack{
         return  i;
     }
 
-    //Explanation: https://www.youtube.com/watch?v=kvyShbFVaY8
+    //Explanation: https://www.youtube.com/watch?v=kvyShbFVaY8\
+        // 0/1 Knapsack
+
+//     private static int knapsackHelper(int target, int[] wt, int[] val, int len){
+//         //Base case
+//         if(len == 0 || target == 0 ){
+//             return  0;
+//         }
+
+
+//         // weight is higher we are not including. recurse remaining elements
+//         if(wt[len-1] > target){
+//             return  knapsackHelper(target, wt,val, len-1);
+//         }else{
+//             // we have choice to include or not include and get max value
+//             return Math.max(val[len-1] +knapsackHelper(target- wt[len-1],wt, val, len-1),  knapsackHelper(target, wt, val, len-1));
+//         }
+//     }
+    
+    static Map<String ,Integer> set = new HashMap<>();
     private static int knapsackHelper(int target, int[] wt, int[] val, int len){
+
         //Base case
         if(len == 0 || target == 0 ){
             return  0;
         }
+        String key = target + "|" + len;
 
 
+        if(set.containsKey(key)){
+            return set.get(key);
+        }
         // weight is higher we are not including. recurse remaining elements
         if(wt[len-1] > target){
-            return  knapsackHelper(target, wt,val, len-1);
+            int x = knapsackHelper(target, wt,val, len-1);
+
+            set.put(key, x);
+            return  x;
         }else{
             // we have choice to include or not include and get max value
-            return Math.max(val[len-1] +knapsackHelper(target- wt[len-1],wt, val, len-1),  knapsackHelper(target, wt, val, len-1));
+            int x =  Math.max(val[len-1] +knapsackHelper(target- wt[len-1],wt, val, len-1),  knapsackHelper(target, wt, val, len-1));
+
+            set.put(key, x);
+
+            return x;
+
+            //            int temp1 = val[len-1] +knapsackHelper(target- wt[len-1],wt, val, len-1);
+//            int temp2 = knapsackHelper(target, wt, val, len-1);
+//            if(temp1>temp2){
+//                return temp1;
+//            }else {
+//                return temp2;
+//            }
+//           // return (temp1>temp2)  ? temp1 :temp1;
+        }
+    }
+
+    private static int knapSackUnbounded(){
+       return 0;
+    }
+
+    public static void main(String[] args){
+        int val[] = { 60, 100, 120, 90, 100, 500, 600, 450 };
+        int wt[] =  { 10, 20,  30,  30, 40,  50 , 60,  70  };
+        int W = 100;
+        int n = val.length;
+        System.out.println(knapSack(W, wt, val, n ));
+        for (Map.Entry<String, Integer> in:set.entrySet()
+             ) {
+            System.out.println(in.getValue());
+
         }
     }
 }
