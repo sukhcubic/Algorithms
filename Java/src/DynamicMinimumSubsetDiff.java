dc3ac473defa9e3c29ad1a7aff4a6944558e22cc
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-09-08 22:58:13 -0500

    Create DynamicMinimumSubsetDiff.java

---
+++ Java/src/DynamicMinimumSubsetDiff.java
@@ -0,0 +1,15 @@
+public class DynamicMinimumSubsetDiff {
+
+
+    private static int minSum(int[] num) {
+        
+        
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4, 10};
+        System.out.println(minSum(num));
+    }
+    
+}
