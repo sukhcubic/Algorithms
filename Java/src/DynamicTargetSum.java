5a9a4a10c9935d2856e95b9dcb56c2775a40ee7a
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-09-12 17:37:19 -0500

    Create DynamicTargetSum.java

---
+++ Java/src/DynamicTargetSum.java
@@ -0,0 +1,85 @@
+public class DynamicTargetSum {
+    //https://leetcode.com/problems/target-sum/
+
+
+    private static int targerSum(int[] items, int sum) {
+       // return targerSumHelper(items, sum, items.length-1);
+        //another way
+
+        
+        // Best mathematical explanation
+        // This is based on Math equation defined in DynamicSubsetDifferenceCount.java
+        int s = 0;
+        for (int x:items
+             ) {
+            s+=x;
+        }
+        s = (s+sum)/2;
+        return targerSumHelper(items, s, items.length-1);
+    }
+
+//    private static int targerSumHelper(int[] items, int sum, int index) {
+//        if(index<0 || sum<0){
+//            return 0;
+//        }
+//        if(sum == 0){
+//            return 1;
+//        }
+//        int rightSubtree =  targerSumHelper(items, sum + items[index], index-1);
+//
+//        int leftSubtree =  targerSumHelper(items, sum-items[index], index-1);
+//
+//        return rightSubtree+leftSubtree;
+//    }
+
+    private static int targerSumHelper(int[] items, int sum, int index) {
+
+        if(sum == 0){
+            return 1;
+        }
+        if(index<0 || sum<0){
+            return 0;
+        }
+        int rightSubtree =  targerSumHelper(items, sum - items[index], index-1);
+
+        int leftSubtree =  targerSumHelper(items, sum, index-1);
+
+        return rightSubtree+leftSubtree;
+    }
+
+    static int count = 0;
+    public static int findTargetSumWays(int[] nums, int S) {
+        calculate(nums, 0, 0, S);
+        return count;
+    }
+    public static void calculate(int[] nums, int i, int sum, int S) {
+        if (i == nums.length) {
+            if (sum == S)
+                count++;
+        } else {
+            calculate(nums, i + 1, sum + nums[i], S);
+            calculate(nums, i + 1, sum - nums[i], S);
+        }
+    }
+
+
+    public static void main(String args[]){
+        int items[] = { 1, 1, 1, 1, 1};
+        int sum = 3;
+
+        //Output should be 5
+        //Explanation from LeetCode
+//        Input: nums is [1, 1, 1, 1, 1], S is 3.
+//        Output: 5
+//        Explanation:
+//        -1+1+1+1+1 = 3;
+//        +1-1+1+1+1 = 3;
+//        +1+1-1+1+1 = 3;
+//        +1+1+1-1+1 = 3;
+//        +1+1+1+1-1 = 3;
+//        There are 5 ways to assign symbols to make the sum of nums be target 3.
+
+        System.out.println(targerSum(items, sum));
+        //System.out.println(findTargetSumWays(items, sum));
+    }
+}
