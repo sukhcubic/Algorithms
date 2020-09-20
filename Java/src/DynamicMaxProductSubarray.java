7bc55862316a36ab462f0d509bac1f9cafb5f527
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-09-20 11:33:51 -0500

    Create DynamicMaxProductSubarray.java

---
+++ Java/src/DynamicMaxProductSubarray.java
@@ -0,0 +1,11 @@
+public class DynamicMaxProductSubarray {
+    static  int maxProduct(int[] nums){
+        return -1;
+    }
+
+    public static void main(String args[]){
+        int items[] = { 1, 5, 10};
+       // int items[] = { 1, -5, 0};
+        System.out.println(maxProduct(items));
+    }
+}
