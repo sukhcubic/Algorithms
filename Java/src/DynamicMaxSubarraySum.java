7f59941e4f9287a3ddced638f43af499a482cd08
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-09-21 23:25:24 -0500

    Update DynamicMaxSubarraySum.java

--- Java/src/DynamicMaxSubarraySum.java
+++ Java/src/DynamicMaxSubarraySum.java
@@ -1,4 +1,12 @@
 class DynamicMaxSubarraySum{
+    private static int maxSum(int[] items) {
+        return 0;
+    }
 
+    public static void main(String args[]){
+        int items[] = { 1, 5, 10};
+        // int items[] = { 1, -5, 0};
+        System.out.println(maxSum(items));
+    }
 
 }
