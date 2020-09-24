a827063ce825b542ff6dc0a73933538c0f8f51bb
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-09-23 22:39:41 -0500

    Update DynamicMaxSubarraySum.java

--- Java/src/DynamicMaxSubarraySum.java
+++ Java/src/DynamicMaxSubarraySum.java
@@ -1,12 +1,20 @@
 class DynamicMaxSubarraySum{
+// Explanation: https://www.youtube.com/watch?v=Id_hZTV7_IA
     private static int maxSum(int[] items) {
-        return 0;
+        int globalSum = 0;
+        int runningSum = 0;
+        for(int i = 0; i<items.length; i++){
+            //Take running sum(sum for previous items) + curr item
+            runningSum = Math.max(runningSum+items[i], items[i]);
+            //then take max of running and global
+            globalSum = Math.max(globalSum, runningSum);
+        }
+        return globalSum;
     }
 
     public static void main(String args[]){
-        int items[] = { 1, 5, 10};
-        // int items[] = { 1, -5, 0};
+        //nt items[] = { 1, 5, 10};
+         int items[] = { 1, -5, 0};
         System.out.println(maxSum(items));
     }
-
 }
