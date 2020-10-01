d40884eacb23db20bfa79df66cb3eb494d00cc23
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-10-01 11:15:25 -0500

    Update DynamicHouseRobber.java

--- Java/src/DynamicHouseRobber.java
+++ Java/src/DynamicHouseRobber.java
@@ -15,7 +15,22 @@ public class DynamicHouseRobber {
         }
         return dp[items.length-1];
     }
+  
+ 
+    // This is quite close to rod cutting problem. Take current and get remaining from item-2. Slit into small sub problems.
+    static int rob1(int[] items){
+        int rob1 = 0;
+        int rob2 = 0;
+        int temp = 0;
 
+        for(int i = 0; i<items.length; i++) {
+            temp = Math.max(rob1, items[i]+rob2);
+            rob2 = rob1;
+            rob1 = temp;
+        }
+        return temp;
+    }
+  
     public static void main(String args[]){
         int items[] = { 2,1,1,2};
         //out 12
