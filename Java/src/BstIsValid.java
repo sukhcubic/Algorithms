d95fdde7aa5cd2d52a48663deb2b6416cdd094c1
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-10-14 18:06:31 -0500

    Update BstIsValid.java

--- Java/src/BstIsValid.java
+++ Java/src/BstIsValid.java
@@ -2,26 +2,26 @@
 //https://www.youtube.com/watch?v=Z_-h_mpDmeg
 //https://www.youtube.com/watch?v=MILxfAbIhrE
 class BstIsValid {
-  public static boolean validateBst(BST tree) {
-
-		if(tree == null){
+ 
+  public boolean isValidBST(TreeNode root) {
+     
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        
+    }
+    
+    boolean helper(TreeNode tree, long min, long max){
+            //Base case null root is valid BST
+       	      if(tree == null){
 			return true;
 		}
-    return isValid(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
-  }
-	
-	public static boolean isValid(BST tree, int min, int max){
-		//Base case empty tree is valid BST
-		if(tree == null){
-			return true;
-		}
-		
-		if(tree.value <= min || tree.value > max){
+		//check if it is between parent + -ve infinty for left and between  parent and +infinity on right sub tree
+		if(tree.val <= min || tree.val >= max){
 			return false;
 		}
-		// tree.value is root value. For left subtree it is -infinity and root value, for right subtree, it is root value and +infinity.
-		return isValid(tree.left, min, tree.value ) && isValid(tree.right, tree.value, max);
-	}
+		// tree.value is root value. For left subtree it is -infinity and root value, for right 
+        //subtree, it is root value and +infinity.
+		return helper(tree.left, min, tree.val ) && helper(tree.right, tree.val, max);
+    }
 	
 //     public static boolean isValid(BST tree, int min, int max){
 		
