09ed41b2e835ef7cc5c9dc8550cadba89600a7dd
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-10-19 21:30:35 -0500

    Create BinarySearch2DMatrix.java

---
+++ Java/src/BinarySearch2DMatrix.java
@@ -0,0 +1,15 @@
+public class BinarySearch2DMatrix {
+     static boolean find(int[][] data, int target){
+         
+         return false;
+     }
+    
+    public static void main(String arg[]){
+        int[][] matrix = {{1,   4,  7, 11, 15},
+                          {2,   5,  8, 12, 19}, 
+                          {3,   6,  9, 16, 22},
+                          {10, 13, 14, 17, 24}, 
+                          {18, 21, 23, 26, 30}};
+        int target = 5;
+    }
+}
