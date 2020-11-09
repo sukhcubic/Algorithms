c228ca82f2ac5c04db52b1b7f0cccc6e50454e26
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-11-09 16:02:29 -0600

    Update BinaryTreeSame.java

--- Java/src/BinaryTreeSame.java
+++ Java/src/BinaryTreeSame.java
@@ -1,1 +1,54 @@
+public class BinaryTreeSame {
 
+    //Recursive
+    static boolean same(TreeNode a, TreeNode b){
+        if(a == null && b == null){
+          return true;
+        }
+        if(a == null || b == null){
+            return false;
+        }
+        if(a.val != b.val){
+          return false;
+        }
+         return same(a.left, b.left) & same(a.right, b.right);
+    }
+
+
+    //Iterative
+    static boolean sameIterative(TreeNode a, TreeNode b){
+
+
+        return false;
+    }
+
+    public static void main(String args[]) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(7);
+        root.right.left = new TreeNode(7);
+
+        TreeNode rootb = new TreeNode(3);
+        rootb.left = new TreeNode(2);
+        rootb.right = new TreeNode(5);
+        rootb.left.left = new TreeNode(1);
+        rootb.left.right = new TreeNode(4);
+        rootb.right.right = new TreeNode(7);
+        rootb.right.left = new TreeNode(7);
+
+
+        TreeNode rootc = new TreeNode(3);
+        rootc.left = new TreeNode(2);
+        rootc.right = new TreeNode(5);
+        rootc.left.left = new TreeNode(1);
+        rootc.left.right = new TreeNode(4);
+        rootc.right.right = new TreeNode(7);
+        rootc.right.left = new TreeNode(8);
+
+        System.out.println(same(root, rootc));
+
+    }
+}
