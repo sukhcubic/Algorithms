2eae4c64ae4f9ed0483e6db04c1b1d8241eae76f
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-11-16 23:56:28 -0600

    Update GraphCourseScheduleREVISIT.java

--- Java/src/GraphCourseScheduleREVISIT.java
+++ Java/src/GraphCourseScheduleREVISIT.java
@@ -2,7 +2,16 @@ class Solution {
 //https://leetcode.com/problems/course-schedule-ii/
 //https://www.youtube.com/watch?v=_LuIvEi_kZk. based on Kahn's algorithm
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        
+//         Algorithm
+// Initialize a queue, Q to keep a track of all the nodes in the graph with 0 in-degree.
+// Iterate over all the edges in the input and create an adjacency list and also a map of node v/s in-degree.
+// Add all the nodes with 0 in-degree to Q.
+// The following steps are to be done until the Q becomes empty.
+// Pop a node from the Q. Let's call this node, N.
+// For all the neighbors of this node, N, reduce their in-degree by 1. If any of the nodes' in-degree reaches 0, add it to the Q.
+// Add the node N to the list maintaining topologically sorted order.
+// Continue from step 4.1.
+
         Map<Integer, List<Integer>> adj = new HashMap<>();
         
         int[] sort = new int[numCourses];
